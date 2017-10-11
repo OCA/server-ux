22ed99d2cc38215fc052e43ed1ca65affb56fc2d
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2017-10-11 11:01:03 +0200

    [MIG] date_range: Adapt JS to new framework

--- date_range/__manifest__.py
+++ date_range/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # © 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Date Range",
     "summary": "Manage all kind of date range",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "category": "Uncategorized",
-    "website": "https://odoo-community.org/",
+    "website": "https://github.com/oca/server-ux",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
