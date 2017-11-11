ae576aeab8b95be59a72cc7c491b18cf4bf3be60
Juan Negrete <negrete.juan85@gmail.com>
2017-11-11 11:41:47 -0600

    [MIG] mass_editing: Migration to 11.0

--- mass_editing/__manifest__.py
+++ mass_editing/__manifest__.py
@@ -1,20 +1,14 @@
-# -*- coding: utf-8 -*-
 # © 2016 Serpent Consulting Services Pvt. Ltd. (support@serpentcs.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Mass Editing',
-    'version': '10.0.1.1.0',
+    'version': '11.0.1.0.0',
     'author': 'Serpent Consulting Services Pvt. Ltd., '
               'Tecnativa, '
               'Odoo Community Association (OCA)',
-    'contributors': [
-        'Oihane Crucelaegui <oihanecrucelaegi@gmail.com>',
-        'Serpent Consulting Services Pvt. Ltd. <support@serpentcs.com>',
-        'Jay Vora <jay.vora@serpentcs.com>'
-    ],
     'category': 'Tools',
     'website': 'http://www.serpentcs.com',
-    'license': 'GPL-3 or any later version',
+    'license': 'AGPL-3',
     'summary': 'Mass Editing',
     'uninstall_hook': 'uninstall_hook',
     'depends': ['base'],
