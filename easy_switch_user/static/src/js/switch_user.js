23689d8f9338041e77c4821b6c02896167ed4f52
tarteo <d.sluijk@onestein.nl>
2018-01-26 12:48:32 +0100

    [FIX] flake

--- easy_switch_user/static/src/js/switch_user.js
+++ easy_switch_user/static/src/js/switch_user.js
@@ -82,7 +82,7 @@ odoo.define('easy_switch_user', function(require) {
                 dialog.open();
             }
         },
-        switch_user: function(login, password, store=false) {
+        switch_user: function(login, password, store = false) {
             var self = this;
             return ajax.jsonRpc('/easy_switch_user/switch', 'call', {
                 login: login,
