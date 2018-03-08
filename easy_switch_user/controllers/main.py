a5324c4bed220125f88acbcfebeaf012a5d17f24
Dennis Sluijk <d.sluijk@onestein.nl>
2018-03-08 11:45:26 +0100

    [11.0][ADD] easy_switch_user (#6)
    * [ADD] easy_switch_user

---
+++ easy_switch_user/controllers/main.py
@@ -0,0 +1,14 @@
+# Copyright 2018 Onestein
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import http
+from odoo.http import route
+
+
+class SwitchController(http.Controller):
+    @route('/easy_switch_user/switch', type='json', auth="none")
+    def switch(self, login, password):
+        request = http.request
+        uid = request.session.authenticate(request.db, login, password)
+        if uid is False:
+            raise Exception('Login Failed')
