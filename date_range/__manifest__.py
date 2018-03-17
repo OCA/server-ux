59088f4d93ba7019bac9b08c95073884c632a5f9
Fekete Mihai <mihai.fekete@forbiom.eu>
2018-03-17 15:48:08 +0200

    Fix pylint warnings.

--- date_range/__manifest__.py
+++ date_range/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # © 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Date Range",
     "summary": "Manage all kind of date range",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "category": "Uncategorized",
     "website": "https://github.com/oca/server-ux",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
