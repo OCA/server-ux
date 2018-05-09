6a5e0085d3a9000fd49949649525c60907f65e21
Lois Rilo <lois.rilo@eficent.com>
2018-05-09 17:08:13 +0200

    [11.0][MIG] base_tier_validation

--- base_tier_validation/__manifest__.py
+++ base_tier_validation/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Base Tier Validation",
     "summary": "Implement a validation process based on tiers.",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Tools",
-    "website": "https://github.com/OCA/server-tools",
+    "website": "https://github.com/OCA/server-ux",
     "author": "Eficent, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
