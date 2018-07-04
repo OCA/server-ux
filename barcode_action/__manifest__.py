8540dade7b0f079053ec2cfc9e664267b7842d3b
Enric Tobella <etobella@creublanca.es>
2018-07-04 15:02:07 +0200

    [IMP] barcode_action: Add tests

--- barcode_action/__manifest__.py
+++ barcode_action/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Barcode action launcher',
     'version': '11.0.1.0.0',
-    'category': 'Reporting',
+    'category': 'Extra Tools',
     'website': 'https://github.com/OCA/server-ux',
     'author': 'Creu Blanca, Eficent, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
--- barcode_action/readme/CONTRIBUTORS.rst
+++ barcode_action/readme/CONTRIBUTORS.rst
@@ -1,2 +1,3 @@
 * Enric Tobella <etobella@creublanca.es>
+* Jordi Ballester <jordi.ballester@eficent.com>
 
