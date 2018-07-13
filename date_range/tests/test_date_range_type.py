058775846e64ce1add7f6ed235f78a7cba7a6bb5
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-07-13 10:03:28 +0200

    [IMP] date_range: Pass integration tests
    If any module adds a required field on company, module fails. Executing it on
    post_install, there's no problem.

--- date_range/tests/test_date_range_type.py
+++ date_range/tests/test_date_range_type.py
@@ -8,6 +8,8 @@ from odoo.exceptions import ValidationError
 
 
 class DateRangeTypeTest(TransactionCase):
+    post_install = True
+    at_install = False
 
     def setUp(self):
         super(DateRangeTypeTest, self).setUp()
