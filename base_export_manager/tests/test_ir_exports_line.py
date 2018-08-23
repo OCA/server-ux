e3be1f98455a9a22679ac29a3b920d280a92840c
Jairo Llopis <yajo.sk8@gmail.com>
2018-08-23 12:33:39 +0100

    [MIG] base_export_manager: Migrate to v11
    - Remove all possible translation commits
    - Aesthetical changes
    - Bump version
    - Some new standards, such as split readme
    
    Basically, nothing.

--- base_export_manager/tests/test_ir_exports_line.py
+++ base_export_manager/tests/test_ir_exports_line.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Antiun Ingenieria S.L. - Javier Iniesta
 # Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
