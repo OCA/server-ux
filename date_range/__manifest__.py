34a037aa77fdc85b67bb16a38666ca6f8b1d4cdf
Andrea <a.stirpe@onestein.nl>
2018-10-01 16:44:07 +0200

    [12.0][MIG] date_range

--- date_range/__manifest__.py
+++ date_range/__manifest__.py
@@ -1,14 +1,13 @@
-# © 2016 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Date Range",
     "summary": "Manage all kind of date range",
-    "version": "11.0.2.0.0",
+    "version": "12.0.1.0.0",
     "category": "Uncategorized",
     "website": "https://github.com/oca/server-ux",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "application": False,
     "installable": True,
     "depends": [
         "web",
@@ -23,6 +22,6 @@
     "qweb": [
         "static/src/xml/date_range.xml",
     ],
-    'development_status': 'Mature',
-    'maintainers': ['lmignon'],
+    "development_status": "Mature",
+    "maintainers": ["lmignon"],
 }
--- date_range/models/date_range_type.py
+++ date_range/models/date_range_type.py
@@ -1,13 +1,13 @@
-# © 2016 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
-from odoo.tools.translate import _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class DateRangeType(models.Model):
     _name = "date.range.type"
+    _description = "Date Range Type"
 
     @api.model
     def _default_company(self):
@@ -18,8 +18,8 @@ class DateRangeType(models.Model):
         help="If sets date range of same type must not overlap.",
         default=False)
     active = fields.Boolean(
-        help="The active field allows you to hide the date range without "
-        "removing it.", default=True)
+        help="The active field allows you to hide the date range type "
+        "without removing it.", default=True)
     company_id = fields.Many2one(
         comodel_name='res.company', string='Company', index=1,
         default=_default_company)
@@ -35,10 +35,10 @@ class DateRangeType(models.Model):
             for rec in self.sudo():
                 if not rec.company_id:
                     continue
-                if bool(self.date_range_ids.filtered(
+                if bool(rec.date_range_ids.filtered(
                         lambda r: r.company_id and
                         r.company_id != rec.company_id)):
                     raise ValidationError(
                         _('You cannot change the company, as this '
                           'Date Range Type is  assigned to Date Range '
-                          '(%s).') % (self.date_range_ids.name_get()[0][1]))
+                          '(%s).') % (rec.date_range_ids.name_get()[0][1]))
