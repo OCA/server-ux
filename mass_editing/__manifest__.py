7db5432d84ed624534b362d3020565f7bfa1a4f6
Aitor Bouzas <bouzasnaveira@gmail.com>
2018-10-08 12:33:21 +0200

    [MIG] mass_editing: Migration to 12.0 Test coverage

--- mass_editing/__manifest__.py
+++ mass_editing/__manifest__.py
@@ -1,8 +1,8 @@
-# © 2016 Serpent Consulting Services Pvt. Ltd. (support@serpentcs.com)
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd. (support@serpentcs.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Mass Editing',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Serpent Consulting Services Pvt. Ltd., '
               'Tecnativa, '
               'Odoo Community Association (OCA)',
