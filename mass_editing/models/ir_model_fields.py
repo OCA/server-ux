29e08fcd7efe4ed46debabef077a77caaf7e04d7
Aitor Bouzas <bouzasnaveira@gmail.com>
2018-10-08 12:33:21 +0200

    [MIG] mass_editing: Migration to 12.0 Test coverage

--- mass_editing/models/ir_model_fields.py
+++ mass_editing/models/ir_model_fields.py
@@ -1,4 +1,4 @@
-# © 2016 Serpent Consulting Services Pvt. Ltd. (support@serpentcs.com)
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd. (support@serpentcs.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
