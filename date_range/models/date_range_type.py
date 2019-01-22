bebde019f6d0e84f4bcf4c488c665c31bbd47e22
Ronald Portier <ronald@therp.nl>
2019-01-22 09:30:57 +0100

    [FIX] date_range. Remove invalid '\xc2' character from headers
    Also changed copyright sign to word Copyright and changed url's to https
    in accordance with latest conventions.
    
    Removed copyright from __init__.py files.

--- date_range/models/date_range_type.py
+++ date_range/models/date_range_type.py
@@ -1,5 +1,5 @@
-# © 2016 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2016 ACSONE SA/NV (<https://acsone.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
 from odoo.tools.translate import _
