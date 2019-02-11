927ea8be51a8817aeadfb8913f501857825aaceb
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-02-11 16:17:27 +0100

    Extract multi_step_wizard as an addon

---
+++ multi_step_wizard/__manifest__.py
@@ -0,0 +1,13 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{'name': 'Multi-Steps Wizards',
+ 'version': '11.0.1.0.0',
+ 'author': 'Camptocamp,Odoo Community Association (OCA)',
+ 'license': 'AGPL-3',
+ 'depends': [],
+ 'website': 'https://github.com/camptocamp/connector-jira',
+ 'data': [
+     'views/multi_step_wizard_views.xml',
+     ],
+ 'installable': True,
+ }
