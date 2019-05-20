56d1cc75173c3cbe3601e829904b3c650d2301bf
Olivier Jossen <olivier.jossen@braintec-group.com>
2019-05-20 16:09:36 +0200

    [MIG] base_export_manager: Migration to 12.0

--- base_export_manager/__manifest__.py
+++ base_export_manager/__manifest__.py
@@ -1,12 +1,13 @@
 # Copyright 2015 Tecnativa - Antonio Espinosa
 # Copyright 2016 Tecnativa - Pedro M. Baeza
 # Copyright 2018 Tecnativa - Jairo Llopis
+# Copyright 2019 brain-tec AG - Olivier Jossen
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': "Manage model export profiles",
     'category': 'Personalization',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'depends': [
         'web',
     ],
@@ -23,8 +24,9 @@
     'author': 'Tecnativa, '
               'LasLabs, '
               'Ursa Information Systems, '
+              'brain-tec AG, '
               'Odoo Community Association (OCA)',
-    'website': 'https://www.tecnativa.com',
+    'website': 'https://github.com/OCA/server-ux',
     'license': 'AGPL-3',
     'installable': True,
     'application': False,
--- base_export_manager/static/src/js/base_export_manager.js
+++ base_export_manager/static/src/js/base_export_manager.js
@@ -1,47 +1,23 @@
-/* Copyright 2015 Antiun Ingenieria, SL (Madrid, Spain, http://www.antiun.com)
+/* Copyright 2019 brain-tec AG (Brig, Switzerland, http://www.braintec-group.com)
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  */
 
 odoo.define('base_export_manager.base_export_manager', function(require) {
     'use strict';
 
-    var core = require('web.core');
-    var Model = require('web.DataModel');
-    var ListView = require('web.ListView');
-    var Sidebar = require('web.Sidebar');
+    var core = require("web.core");
+    var Sidebar = require("web.Sidebar");
+    var session = require("web.session");
     var _t = core._t;
 
-    ListView.include({
-        /**
-         * Instantiate and render the sidebar.
-         * Sets this.sidebar
-         * @param {jQuery} [$node] a jQuery node where the sidebar should be inserted
-         * $node may be undefined, in which case the ListView inserts the sidebar in
-         * this.options.$sidebar or in a div of its template
-         **/
-        render_sidebar: function($node) {
-            var self = this;
-            this._super($node);
-            var Users = new Model('res.users');
-            Users.call('fetch_export_models', []).done(function(export_models){
-                self.export_models = export_models;
-                self.render_export_enable = jQuery.inArray( self.model, self.export_models );
-                    if(self.sidebar && self.sidebar.items && self.sidebar.items.other){
-                        var items_data = [];
-                        _.each(self.sidebar.items.other,function(rec){
-                            if(rec.label != _t("Export")){
-                                items_data.push(rec);
-                            }
-                        });
-                        self.sidebar.items.other = items_data;
-                        self.sidebar.add_items('other', _.compact([
-                            self.render_export_enable >= 0 && {
-                                label: _t("Export"),
-                                callback: self.on_sidebar_export
-                            },
-                        ]));
-                     }
-            });
+    Sidebar.include({
+        _addItems: function (sectionCode, items) {
+            var _items = items;
+            var render_export_enalble = jQuery.inArray( this.env.model, session.export_models)
+            if (!session.is_superuser && sectionCode === 'other' && items.length && render_export_enalble < 0) {
+                _items = _.reject(_items, {label:_t("Export")});
+            }
+            this._super(sectionCode, _items);
         },
     });
 });
