4936e07a64a54c934631a024e495c4e9715ec406
Olivier Jossen <olivier.jossen@braintec-group.com>
2019-05-20 16:09:36 +0200

    [MIG] base_export_manager: Migration to 12.0

---
+++ base_export_manager/models/ir_http.py
@@ -0,0 +1,18 @@
+# Copyright 2019 brain-tec AG - Olivier Jossen
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+from odoo.http import request
+
+
+class Http(models.AbstractModel):
+    _inherit = 'ir.http'
+
+    def session_info(self):
+        res = super(Http, self).session_info()
+        user = request.env.user
+        export_models = user.fetch_export_models()
+        res.update({
+            'export_models': export_models,
+        })
+        return res
