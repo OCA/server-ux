ff967c44c2dab8b8e8c0056ecfa7bf47fe37cb47
Lois Rilo <lois.rilo@eficent.com>
2019-05-28 14:33:47 +0200

    [12.0][MIG] base_tier_validation_formula

--- base_tier_validation_formula/__manifest__.py
+++ base_tier_validation_formula/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Base Tier Validation Formula',
     'summary': """
         Formulas for Base tier validation""",
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Creu Blanca,Odoo Community Association (OCA)',
     'website': 'www.creublanca.es',
