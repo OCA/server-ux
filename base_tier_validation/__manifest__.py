648a8ad5fe44177d7835ad9e4af700cda5df69d0
Florent de Labarre <florent.mirieu@gmail.com>
2019-07-04 21:46:38 +0200

    base_tier_validation : singleton error

--- base_tier_validation/__manifest__.py
+++ base_tier_validation/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Base Tier Validation",
     "summary": "Implement a validation process based on tiers.",
-    "version": "12.0.3.0.0",
+    "version": "12.0.3.1.0",
     "development_status": "Mature",
     "maintainers": ['lreficent'],
     "category": "Tools",
