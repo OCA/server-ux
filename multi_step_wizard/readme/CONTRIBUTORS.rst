028d389b05f696f176edfb0ef71d66c3bbf6b818
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-07-20 12:35:48 +0300

    [MIG] multi_step_wizard: Migration to 12.0

---
+++ multi_step_wizard/readme/CONTRIBUTORS.rst
@@ -0,0 +1,2 @@
+* Guewen Baconnier <guewen.baconnier@camptocamp.com>
+* Alexey Pelykh <alexey.pelykh@brainbeanapps.com>
---
+++ multi_step_wizard/tests/test_multi_step_wizard.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo.tests.common import TransactionCase
+
+
+class TestMultiStepWizard(TransactionCase):
+
+    def setUp(self):
+        super().setUp()
+        self.MultiStepWizard = self.env['multi.step.wizard.test']
+
+    def test_behavior(self):
+        wizard = self.MultiStepWizard.create({})
+        wizard.open_next()
+        self.assertEqual(wizard.state, 'final')
+        with self.assertRaises(NotImplementedError):
+            wizard.open_next()
