d44b3c183d6ea489a2ee44e6052404e4b41eb8bc
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-09-04 19:43:23 +0200

    [ADD] base_user_locale: configure locale settings

---
+++ base_user_locale/models/res_company.py
@@ -0,0 +1,29 @@
+# Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    date_format = fields.Char(
+        string='Date Format',
+        help='See Settings > Translations > Languages for details',
+    )
+    time_format = fields.Char(
+        string='Time Format',
+        help='See Settings > Translations > Languages for details',
+    )
+    week_start = fields.Selection(
+        string='Week Start',
+        selection=[
+            ('1', 'Monday'),
+            ('2', 'Tuesday'),
+            ('3', 'Wednesday'),
+            ('4', 'Thursday'),
+            ('5', 'Friday'),
+            ('6', 'Saturday'),
+            ('7', 'Sunday'),
+        ],  # NOTE: Numbers as strings otherwise UI works poorly
+    )
