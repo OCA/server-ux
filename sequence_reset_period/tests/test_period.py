5fcdf2818cde73e87fe29f6d03241dcad16e8f1a
Jaume Planas <jaume.planas@minorisa.net>
2019-09-27 11:51:09 +0200

    [MIG] sequence_reset_period: Migration to 12.0

--- sequence_reset_period/tests/test_period.py
+++ sequence_reset_period/tests/test_period.py
@@ -3,13 +3,13 @@
 
 
 from odoo.tests import common
-from datetime import datetime
+from datetime import date
 
 
 class TestSequence(common.TransactionCase):
     def setUp(self):
         super().setUp()
-        self.date = datetime(2018, 3, 14).strftime('%Y-%m-%d')
+        self.date = date(2018, 3, 14)
 
     def get_sequence(self, method):
         return self.env['ir.sequence'].create({
@@ -25,47 +25,62 @@ class TestSequence(common.TransactionCase):
         self.assertFalse(sequence.date_range_ids)
         self.assertEqual('00001', sequence.with_context(
             ir_sequence_date=self.date).next_by_id())
-        range = sequence.date_range_ids
-        self.assertTrue(range)
-        self.assertEqual('2018-01-01', range.date_from)
-        self.assertEqual('2018-12-31', range.date_to)
+        xrange = sequence.date_range_ids
+        self.assertTrue(xrange)
+        self.assertEqual(date(2018, 1, 1), xrange.date_from)
+        self.assertEqual(date(2018, 12, 31), xrange.date_to)
 
     def test_daily(self):
         sequence = self.get_sequence('daily')
         self.assertFalse(sequence.date_range_ids)
         self.assertEqual('00001', sequence.with_context(
             ir_sequence_date=self.date).next_by_id())
-        range = sequence.date_range_ids
-        self.assertTrue(range)
-        self.assertEqual(self.date, range.date_from)
-        self.assertEqual(self.date, range.date_to)
+        xrange = sequence.date_range_ids
+        self.assertTrue(xrange)
+        self.assertEqual(self.date, xrange.date_from)
+        self.assertEqual(self.date, xrange.date_to)
 
     def test_weekly(self):
         sequence = self.get_sequence('weekly')
         self.assertFalse(sequence.date_range_ids)
         self.assertEqual('00001', sequence.with_context(
             ir_sequence_date=self.date).next_by_id())
-        range = sequence.date_range_ids
-        self.assertTrue(range)
-        self.assertEqual('2018-03-12', range.date_from)
-        self.assertEqual('2018-03-18', range.date_to)
+        xrange = sequence.date_range_ids
+        self.assertTrue(xrange)
+        self.assertEqual(date(2018, 3, 12), xrange.date_from)
+        self.assertEqual(date(2018, 3, 18), xrange.date_to)
 
     def test_monthly(self):
         sequence = self.get_sequence('monthly')
         self.assertFalse(sequence.date_range_ids)
         self.assertEqual('00001', sequence.with_context(
             ir_sequence_date=self.date).next_by_id())
-        range = sequence.date_range_ids
-        self.assertTrue(range)
-        self.assertEqual('2018-03-01', range.date_from)
-        self.assertEqual('2018-03-31', range.date_to)
+        xrange = sequence.date_range_ids
+        self.assertTrue(xrange)
+        self.assertEqual(date(2018, 3, 1), xrange.date_from)
+        self.assertEqual(date(2018, 3, 31), xrange.date_to)
 
     def test_yearly(self):
         sequence = self.get_sequence('yearly')
         self.assertFalse(sequence.date_range_ids)
         self.assertEqual('00001', sequence.with_context(
             ir_sequence_date=self.date).next_by_id())
-        range = sequence.date_range_ids
-        self.assertTrue(range)
-        self.assertEqual('2018-01-01', range.date_from)
-        self.assertEqual('2018-12-31', range.date_to)
+        xrange = sequence.date_range_ids
+        self.assertTrue(xrange)
+        self.assertEqual(date(2018, 1, 1), xrange.date_from)
+        self.assertEqual(date(2018, 12, 31), xrange.date_to)
+
+    def test_monthly_existing(self):
+        sequence = self.get_sequence('monthly')
+        self.env['ir.sequence.date_range'].create({
+            'date_from': date(2018, 3, 1),
+            'date_to': date(2018, 3, 10),
+            'sequence_id': sequence.id,
+        })
+        self.env['ir.sequence.date_range'].create({
+            'date_from': date(2018, 3, 20),
+            'date_to': date(2018, 3, 25),
+            'sequence_id': sequence.id,
+        })
+        self.assertEqual('00001', sequence.with_context(
+            ir_sequence_date=self.date).next_by_id())
