371a8ab8df506c509b6118e9ce9ce93d6d3edb55
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-09-30 11:10:45 +0200

    [PORT] mass_editing in V13 [REF] Update mass_operation_abstract/wizard/mass_operation_wizard_mixin.py Co-Authored-By: David Beal <david.beal@akretion.com>

---
+++ mass_editing/migrations/13.0.1.0.0/post-migration.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2019 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+def migrate(cr, version):
+    if not version:
+        return
+
+    # Rename table for consistency reason
+    cr.execute(
+        """
+        INSERT INTO mass_editing_line (mass_editing_id, field_id)
+            SELECT
+                mass_id as mass_editing_id,
+                field_id
+            FROM mass_field_rel;
+    """
+    )
