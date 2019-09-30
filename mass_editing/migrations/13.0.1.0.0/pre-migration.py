10cd979dfa13ffb56d09fbc8449372dce55403a6
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-09-30 11:10:45 +0200

    [PORT] mass_editing in V13 [REF] Update mass_operation_abstract/wizard/mass_operation_wizard_mixin.py Co-Authored-By: David Beal <david.beal@akretion.com>

---
+++ mass_editing/migrations/13.0.1.0.0/pre-migration.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2019 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+def migrate(cr, version):
+    if not version:
+        return
+
+    # First remove the obsolete constraint created by the existence of the
+    # remove M2M field 'field_ids' between mass.object and ir.model.fields
+    cr.execute(
+        """
+        DELETE FROM ir_model_relation
+        WHERE name = 'mass_field_rel';
+        """
+    )
+    # Rename table for consistency reason
+    cr.execute(
+        """
+        ALTER TABLE mass_object
+        RENAME TO mass_editing;
+    """
+    )
+    # Create and Compute new required field
+    cr.execute(
+        """
+        ALTER TABLE mass_editing
+        ADD COLUMN action_name varchar;
+    """
+    )
+    cr.execute(
+        """
+        UPDATE mass_editing
+        SET action_name = name;
+    """
+    )
+    cr.execute(
+        """
+        alter sequence mass_object_id_seq rename to mass_editing_id_seq;"""
+    )
