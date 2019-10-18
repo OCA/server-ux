cfda356a2432fb659f8ac19511d05a50d1826518
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-10-18 12:15:43 +0200

    [FIX] black, isort, eslint

--- date_range/tests/test_date_range_generator.py
+++ date_range/tests/test_date_range_generator.py
@@ -10,38 +10,38 @@ from odoo.tests.common import TransactionCase
 
 
 class DateRangeGeneratorTest(TransactionCase):
-
     def setUp(self):
         super(DateRangeGeneratorTest, self).setUp()
-        self.generator = self.env['date.range.generator']
-        self.type = self.env['date.range.type'].create(
-            {'name': 'Fiscal year',
-             'company_id': False,
-             'allow_overlap': False})
+        self.generator = self.env["date.range.generator"]
+        self.type = self.env["date.range.type"].create(
+            {"name": "Fiscal year", "company_id": False, "allow_overlap": False}
+        )
 
-        self.company = self.env['res.company'].create({
-            'name': 'Test company',
-        })
-        self.company_2 = self.env['res.company'].create({
-            'name': 'Test company 2',
-            'parent_id': self.company.id,
-        })
-        self.typeB = self.env['date.range.type'].create(
-            {'name': 'Fiscal year B',
-             'company_id': self.company.id,
-             'allow_overlap': False})
+        self.company = self.env["res.company"].create({"name": "Test company"})
+        self.company_2 = self.env["res.company"].create(
+            {"name": "Test company 2", "parent_id": self.company.id}
+        )
+        self.typeB = self.env["date.range.type"].create(
+            {
+                "name": "Fiscal year B",
+                "company_id": self.company.id,
+                "allow_overlap": False,
+            }
+        )
 
     def test_generate(self):
-        generator = self.generator.create({
-            'date_start': '1943-01-01',
-            'name_prefix': '1943-',
-            'type_id': self.type.id,
-            'duration_count': 3,
-            'unit_of_time': str(MONTHLY),
-            'count': 4})
+        generator = self.generator.create(
+            {
+                "date_start": "1943-01-01",
+                "name_prefix": "1943-",
+                "type_id": self.type.id,
+                "duration_count": 3,
+                "unit_of_time": str(MONTHLY),
+                "count": 4,
+            }
+        )
         generator.action_apply()
-        ranges = self.env['date.range'].search(
-            [('type_id', '=', self.type.id)])
+        ranges = self.env["date.range"].search([("type_id", "=", self.type.id)])
         self.assertEqual(len(ranges), 4)
         range4 = ranges[3]
         self.assertEqual(range4.date_start, datetime.date(1943, 10, 1))
@@ -50,12 +50,14 @@ class DateRangeGeneratorTest(TransactionCase):
 
     def test_generator_multicompany_1(self):
         with self.assertRaises(ValidationError):
-            self.generator.create({
-                'date_start': '1943-01-01',
-                'name_prefix': '1943-',
-                'type_id': self.typeB.id,
-                'duration_count': 3,
-                'unit_of_time': str(MONTHLY),
-                'count': 4,
-                'company_id': self.company_2.id,
-            })
+            self.generator.create(
+                {
+                    "date_start": "1943-01-01",
+                    "name_prefix": "1943-",
+                    "type_id": self.typeB.id,
+                    "duration_count": 3,
+                    "unit_of_time": str(MONTHLY),
+                    "count": 4,
+                    "company_id": self.company_2.id,
+                }
+            )
