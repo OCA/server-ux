a5832f68ebc34e162dd636128a4d75df8b8eaf4f
ps-tubtim <pimolnat.s@outlook.com>
2019-11-26 17:59:25 +0700

    [12.0][IMP] base_tier_validation

--- base_tier_validation/models/tier_review.py
+++ base_tier_validation/models/tier_review.py
@@ -41,6 +41,17 @@ class TierReview(models.Model):
         comodel_name="res.users",
     )
     reviewed_date = fields.Datetime(string='Validation Date')
+    has_comment = fields.Boolean(
+        related='definition_id.has_comment',
+        readonly=True,
+    )
+    comment = fields.Char(
+        string='Comments',
+    )
+    approve_sequence = fields.Boolean(
+        related='definition_id.approve_sequence',
+        readonly=True,
+    )
 
     @api.model
     def _get_reviewer_fields(self):
