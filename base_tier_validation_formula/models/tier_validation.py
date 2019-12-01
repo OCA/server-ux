4f52d2f64bb9ea6ec6933991b4df634c17bc08b9
pedro.gonzalez@pesol.es <pedro.gonzalez@pesol.es>
2019-12-01 14:22:54 +0100

    [IMP] base_tier_validation_formula: black, isort

--- base_tier_validation_formula/models/tier_validation.py
+++ base_tier_validation_formula/models/tier_validation.py
@@ -1,25 +1,26 @@
 # Copyright 2019 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models, _
-from odoo.tools.safe_eval import safe_eval
+from odoo import _, api, models
 from odoo.exceptions import UserError
+from odoo.tools.safe_eval import safe_eval
 
 
 class TierValidation(models.AbstractModel):
-    _inherit = 'tier.validation'
+    _inherit = "tier.validation"
 
     @api.multi
     def evaluate_formula_tier(self, tier):
         try:
-            res = safe_eval(tier.python_code, globals_dict={'rec': self})
+            res = safe_eval(tier.python_code, globals_dict={"rec": self})
         except Exception as error:
-            raise UserError(_(
-                "Error evaluating tier validation conditions.\n %s") % error)
+            raise UserError(
+                _("Error evaluating tier validation conditions.\n %s") % error
+            )
         return res
 
     @api.multi
     def evaluate_tier(self, tier):
-        if tier.definition_type == 'formula':
+        if tier.definition_type == "formula":
             return self.evaluate_formula_tier(tier)
         return super().evaluate_tier(tier)
