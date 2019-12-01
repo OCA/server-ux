7394152070804941c3f9b34f38010158826fde15
pedro.gonzalez@pesol.es <pedro.gonzalez@pesol.es>
2019-12-01 14:22:54 +0100

    [IMP] base_tier_validation_formula: black, isort

--- base_tier_validation_formula/__manifest__.py
+++ base_tier_validation_formula/__manifest__.py
@@ -2,19 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Base Tier Validation Formula',
-    'summary': """
+    "name": "Base Tier Validation Formula",
+    "summary": """
         Formulas for Base tier validation""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/server-ux',
-    'depends': [
-        'base_tier_validation'
-    ],
-    'data': [
-        'views/tier_definition_view.xml',
-    ],
-    'demo': [
-    ],
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-ux",
+    "depends": ["base_tier_validation"],
+    "data": ["views/tier_definition_view.xml"],
+    "demo": [],
 }
--- base_tier_validation_formula/models/tier_definition.py
+++ base_tier_validation_formula/models/tier_definition.py
@@ -1,37 +1,35 @@
 # Copyright 2019 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, api, models
+from odoo import api, fields, models
 
 
 class TierDefinition(models.Model):
-    _inherit = 'tier.definition'
+    _inherit = "tier.definition"
 
     python_code = fields.Text(
-        string='Tier Definition Expression',
+        string="Tier Definition Expression",
         help="Write Python code that defines when this tier confirmation "
-             "will be needed. The result of executing the expresion must be "
-             "a boolean.",
+        "will be needed. The result of executing the expresion must be "
+        "a boolean.",
         default="""# Available locals:\n#  - rec: current record""",
     )
-    definition_type = fields.Selection(
-        selection_add=[('formula', 'Formula')]
-    )
+    definition_type = fields.Selection(selection_add=[("formula", "Formula")])
     reviewer_expression = fields.Text(
-        string='Review Expression',
+        string="Review Expression",
         help="Write Python code that defines the reviewer. "
-             "The result of executing the expression must be a res.users "
-             "recordset.",
+        "The result of executing the expression must be a res.users "
+        "recordset.",
         default="# Available locals:\n#  - rec: current record\n"
-                "#  - Expects a recordset of res.users",
+        "#  - Expects a recordset of res.users",
     )
-    review_type = fields.Selection(
-        selection_add=[("expression", "Python Expression")]
-    )
+    review_type = fields.Selection(selection_add=[("expression", "Python Expression")])
 
-    @api.onchange('review_type')
+    @api.onchange("review_type")
     def onchange_review_type(self):
         super(TierDefinition, self).onchange_review_type()
-        self.reviewer_expression = "# Available locals:\n" \
-                                   "#  - rec: current record\n" \
-                                   "#  - Expects a recordset of res.users"
+        self.reviewer_expression = (
+            "# Available locals:\n"
+            "#  - rec: current record\n"
+            "#  - Expects a recordset of res.users"
+        )
