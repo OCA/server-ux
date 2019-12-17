9d96225e7f28a3ba21dfaf6f3b785ac597641890
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-12-17 19:44:30 +0100

    [WIP] DO NOT MERGE: Test for unrelated dependencies

---
+++ mrp_test/__manifest__.py
@@ -0,0 +1,11 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'MRP Test',
+    'version': '12.0.1.0.0',
+    'author': 'Odoo Community Association (OCA)',
+    'category': 'Tools',
+    'website': 'https://github.com/OCA/server-ux',
+    'license': 'AGPL-3',
+    'depends': ['mrp'],
+    'data': [],
+}
