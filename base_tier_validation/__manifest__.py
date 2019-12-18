17da52b2e256745c1b59f57c29457ea2e4c1ba89
Lois Rilo <lois.rilo@forgeflow.com>
2019-12-18 12:13:10 +0100

    [13.0][UPD] base_tier_validation: update copyrights information after rebranding of Eficent to ForgeFlow.

--- base_tier_validation/__manifest__.py
+++ base_tier_validation/__manifest__.py
@@ -1,14 +1,14 @@
-# Copyright 2017-19 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-19 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Base Tier Validation",
     "summary": "Implement a validation process based on tiers.",
     "version": "13.0.1.0.0",
     "development_status": "Mature",
-    "maintainers": ["lreficent"],
+    "maintainers": ["LoisRForgeFlow"],
     "category": "Tools",
     "website": "https://github.com/OCA/server-ux",
-    "author": "Eficent, Odoo Community Association (OCA)",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
