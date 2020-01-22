4bedd162653ea944758e31fd8762e90e1d2808ba
jim.hoefnagels <jim.hoefnagels@dynapps.be>
2020-01-22 13:33:37 +0100

    [IMP] solve pre commit warnings

--- base_tier_validation/static/src/js/systray.js
+++ base_tier_validation/static/src/js/systray.js
@@ -133,9 +133,11 @@ odoo.define('tier_validation.systray', function (require) {
                 res_model: data.res_model,
                 views: [[false, 'list'], [false, 'form']],
                 search_view_id: [false],
-                domain: [['review_ids.reviewer_ids', '=', session.uid],
-                         ['review_ids.status', '=', 'pending'],
-                         ['review_ids.can_review', '=', true]],
+                domain: [
+                    ['review_ids.reviewer_ids', '=', session.uid],
+                    ['review_ids.status', '=', 'pending'],
+                    ['review_ids.can_review', '=', true],
+                ],
                 context:context,
             });
         },
