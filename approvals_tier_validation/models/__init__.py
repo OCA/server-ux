c44395ca379c4bbbfeaf4dfc734f121e3e03cff5
kittiu <kittiu@gmail.com>
2020-02-01 23:22:20 +0700

    [13.0][ADD] approvals_tier_validation

---
+++ approvals_tier_validation/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2020 Ecosoft Co., Ltd.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import tier_definition
+from . import tier_approval_request
+from . import tier_approval_category
