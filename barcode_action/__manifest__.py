7c12cc731f1a0ef569665a3590a90f6da7e12a03
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2020-03-09 15:18:34 +0100

    [IMP] barcode_action: black, isort

--- barcode_action/__manifest__.py
+++ barcode_action/__manifest__.py
@@ -3,23 +3,16 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'Barcode action launcher',
-    'version': '12.0.1.0.0',
-    'category': 'Extra Tools',
-    'website': 'https://github.com/OCA/server-ux',
-    'author': 'Creu Blanca, Eficent, Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
-    'summary': 'Allows to use barcodes as a launcher',
-    'depends': [
-        'barcodes',
-    ],
-    'data': [
-        'views/barcode_templates.xml',
-        'wizard/barcode_action_view.xml',
-    ],
-    'demo': [
-        'demo/barcode_action_demo.xml'
-    ],
+    "name": "Barcode action launcher",
+    "version": "12.0.1.0.0",
+    "category": "Extra Tools",
+    "website": "https://github.com/OCA/server-ux",
+    "author": "Creu Blanca, Eficent, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "Allows to use barcodes as a launcher",
+    "depends": ["barcodes"],
+    "data": ["views/barcode_templates.xml", "wizard/barcode_action_view.xml"],
+    "demo": ["demo/barcode_action_demo.xml"],
 }
