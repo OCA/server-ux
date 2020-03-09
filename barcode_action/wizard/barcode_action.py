40b9bf7618fda07cb2b28c1168356b7b3ae5a2fd
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2020-03-09 15:18:34 +0100

    [IMP] barcode_action: black, isort

--- barcode_action/wizard/barcode_action.py
+++ barcode_action/wizard/barcode_action.py
@@ -2,21 +2,15 @@ from odoo import fields, models
 
 
 class BarcodeAction(models.TransientModel):
-    _name = 'barcode.action'
+    _name = "barcode.action"
     _inherit = "barcodes.barcode_events_mixin"
 
-    model = fields.Char(
-        required=True, readonly=True
-    )
+    model = fields.Char(required=True, readonly=True)
     res_id = fields.Integer()
-    method = fields.Char(
-        required=True, readonly=True
-    )
-    state = fields.Selection([
-        ('waiting', 'Waiting'),
-        ('warning', 'Warning')
-    ], default='waiting', readonly=True, )
-    status = fields.Text(
+    method = fields.Char(required=True, readonly=True)
+    state = fields.Selection(
+        [("waiting", "Waiting"), ("warning", "Warning")],
+        default="waiting",
         readonly=True,
-        default="Start scanning",
     )
+    status = fields.Text(readonly=True, default="Start scanning")
