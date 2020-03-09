93fdd60f24fe5832f3253fdec61f2aed5015ce8b
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2020-03-09 15:18:34 +0100

    [IMP] barcode_action: black, isort

--- barcode_action/tests/test_partner_find.py
+++ barcode_action/tests/test_partner_find.py
@@ -5,20 +5,18 @@ from odoo.tests.common import TransactionCase
 
 
 class TestPartnerFind(TransactionCase):
-
     def test_partner(self):
-        partner_obj = self.env['res.partner']
-        ref = 'testing_partner_internal_reference'
-        partner = partner_obj.create({
-            'name': 'Testing partner',
-            'ref': ref,
-        })
+        partner_obj = self.env["res.partner"]
+        ref = "testing_partner_internal_reference"
+        partner = partner_obj.create({"name": "Testing partner", "ref": ref})
         # We should find the partner when the ref is found
         self.assertEqual(
             partner.id,
-            partner_obj.find_res_partner_by_ref_using_barcode(ref).get(
-                'res_id', False))
+            partner_obj.find_res_partner_by_ref_using_barcode(ref).get("res_id", False),
+        )
         # No partner is found, then there is no res_id on the result
         self.assertFalse(
-            partner_obj.find_res_partner_by_ref_using_barcode(
-                '%s-%s' % (ref, ref)).get('res_id', False))
+            partner_obj.find_res_partner_by_ref_using_barcode("{}-{}".format(ref, ref)).get(
+                "res_id", False
+            )
+        )
