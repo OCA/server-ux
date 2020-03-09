a4f89dcce25e2dfc852a225bd31f19d24b226189
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2020-03-09 15:18:34 +0100

    [IMP] barcode_action: black, isort

--- barcode_action/models/res_partner.py
+++ barcode_action/models/res_partner.py
@@ -2,30 +2,35 @@
 # Copyright 2018 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 import json
-from odoo import api, models, _
+
+from odoo import _, api, models
 from odoo.tools.safe_eval import safe_eval
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     @api.multi
     def find_res_partner_by_ref_using_barcode(self, barcode):
-        partner = self.search([('ref', '=', barcode)], limit=1)
+        partner = self.search([("ref", "=", barcode)], limit=1)
         if not partner:
-            action = self.env.ref('barcode_action.res_partner_find')
+            action = self.env.ref("barcode_action.res_partner_find")
             result = action.read()[0]
-            context = safe_eval(result['context'])
-            context.update({
-                'default_state': 'warning',
-                'default_status': _('Partner with Internal Reference '
-                                    '%s cannot be found') % barcode
-            })
-            result['context'] = json.dumps(context)
+            context = safe_eval(result["context"])
+            context.update(
+                {
+                    "default_state": "warning",
+                    "default_status": _(
+                        "Partner with Internal Reference " "%s cannot be found"
+                    )
+                    % barcode,
+                }
+            )
+            result["context"] = json.dumps(context)
             return result
-        action = self.env.ref('base.action_partner_form')
+        action = self.env.ref("base.action_partner_form")
         result = action.read()[0]
-        res = self.env.ref('base.view_partner_form', False)
-        result['views'] = [(res and res.id or False, 'form')]
-        result['res_id'] = partner.id
+        res = self.env.ref("base.view_partner_form", False)
+        result["views"] = [(res and res.id or False, "form")]
+        result["res_id"] = partner.id
         return result
