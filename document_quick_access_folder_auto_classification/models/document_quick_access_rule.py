ad4ba72e659ff7104b24de0c8205be39f654935e
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-12 10:39:58 +0100

    [IMP] document_quick_access_folder_auto_classification: black, isort

--- document_quick_access_folder_auto_classification/models/document_quick_access_rule.py
+++ document_quick_access_folder_auto_classification/models/document_quick_access_rule.py
@@ -1,15 +1,18 @@
 # Copyright 2019 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-import os
 import base64
+import logging
+import os
 import shutil
+
 from odoo import api, models
+from odoo.modules.registry import Registry
+
 try:
     from odoo.addons.queue_job.job import job
 except ImportError:
     job = lambda *args, **kwargs: lambda func: func  # noqa: E731
-from odoo.modules.registry import Registry
-import logging
+
 _logger = logging.getLogger(__name__)
 
 
@@ -19,29 +22,36 @@ class OCRException(Exception):
 
 
 class DocumentQuickAccessRule(models.Model):
-    _inherit = 'document.quick.access.rule'
+    _inherit = "document.quick.access.rule"
 
     @api.model
     def cron_folder_auto_classification(
         self, path=False, processing_path=False, limit=False
     ):
         if not path:
-            path = self.env['ir.config_parameter'].sudo().get_param(
-                'document_quick_access_auto_classification.path',
-                default=False)
+            path = (
+                self.env["ir.config_parameter"]
+                .sudo()
+                .get_param(
+                    "document_quick_access_auto_classification.path", default=False
+                )
+            )
         if not path:
             return False
-        if not processing_path and not self.env.context.get(
-            'ignore_process_path'
-        ):
-            processing_path = self.env[
-                'ir.config_parameter'
-            ].sudo().get_param(
-                'document_quick_access_auto_classification.process_path',
-                default=False)
-        elements = [os.path.join(
-            path, f
-        ) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+        if not processing_path and not self.env.context.get("ignore_process_path"):
+            processing_path = (
+                self.env["ir.config_parameter"]
+                .sudo()
+                .get_param(
+                    "document_quick_access_auto_classification.process_path",
+                    default=False,
+                )
+            )
+        elements = [
+            os.path.join(path, f)
+            for f in os.listdir(path)
+            if os.path.isfile(os.path.join(path, f))
+        ]
         if limit:
             elements = elements[:limit]
         for element in elements:
@@ -52,11 +62,16 @@ class DocumentQuickAccessRule(models.Model):
             try:
                 if processing_path:
                     new_element = os.path.join(
-                        processing_path, os.path.basename(element))
+                        processing_path, os.path.basename(element)
+                    )
                     shutil.copy(element, new_element)
-                    obj = api.Environment(
-                        new_cr, self.env.uid, self.env.context
-                    )[self._name].browse().with_delay(**self._delay_vals())
+                    obj = (
+                        api.Environment(new_cr, self.env.uid, self.env.context)[
+                            self._name
+                        ]
+                        .browse()
+                        .with_delay(**self._delay_vals())
+                    )
                 obj._process_document(new_element)
                 if processing_path:
                     new_cr.commit()
@@ -78,55 +93,59 @@ class DocumentQuickAccessRule(models.Model):
 
     @api.model
     def process_document(self, filename, datas):
-        attachment = self.env['ir.attachment'].create(
+        attachment = self.env["ir.attachment"].create(
             self._get_attachment_vals(filename, datas)
         )
         return attachment._process_quick_access_rules()
 
     @api.model
-    @job(default_channel='root.document_quick_access_classification')
+    @job(default_channel="root.document_quick_access_classification")
     def _process_document(self, element):
         try:
             filename = os.path.basename(element)
-            datas = base64.b64encode(open(element, 'rb').read())
+            datas = base64.b64encode(open(element, "rb").read())
             results = self.process_document(filename, datas)
             return self._postprocess_document(element, results)
         except OCRException:
-            _logger.warning('Element %s was corrupted' % element)
+            _logger.warning("Element %s was corrupted" % element)
             os.unlink(element)
 
     @api.model
     def _postprocess_document(self, path, results):
         filename = os.path.basename(path)
         if any(result.res_id for result in results):
-            new_path = self.env['ir.config_parameter'].sudo().get_param(
-                'document_quick_access_auto_classification.ok_path',
-                default=False)
+            new_path = (
+                self.env["ir.config_parameter"]
+                .sudo()
+                .get_param(
+                    "document_quick_access_auto_classification.ok_path", default=False
+                )
+            )
         else:
-            new_path = self.env['ir.config_parameter'].sudo().get_param(
-                'document_quick_access_auto_classification.failure_path',
-                default=False)
-            self.env['document.quick.access.missing'].create({
-                'name': filename,
-                'attachment_id': results.id
-            })
+            new_path = (
+                self.env["ir.config_parameter"]
+                .sudo()
+                .get_param(
+                    "document_quick_access_auto_classification.failure_path",
+                    default=False,
+                )
+            )
+            self.env["document.quick.access.missing"].create(
+                {"name": filename, "attachment_id": results.id}
+            )
         if new_path:
             shutil.copy(path, os.path.join(new_path, filename))
         os.unlink(path)
         return bool(results)
 
     def _get_attachment_vals(self, filename, datas):
-        return {
-            'name': filename,
-            'datas': datas,
-            'datas_fname': filename,
-        }
+        return {"name": filename, "datas": datas, "datas_fname": filename}
 
     @api.model
     def read_code(self, code):
         try:
             return super().read_code(code)
         except Exception:
-            if self.env.context.get('no_raise_document_access', False):
+            if self.env.context.get("no_raise_document_access", False):
                 return False
             raise
--- document_quick_access_folder_auto_classification/wizards/document_quick_access_missing_assign.py
+++ document_quick_access_folder_auto_classification/wizards/document_quick_access_missing_assign.py
@@ -6,30 +6,25 @@ from odoo import api, fields, models
 
 class DocumentQuickAccessMissingAssign(models.TransientModel):
 
-    _name = 'document.quick.access.missing.assign'
+    _name = "document.quick.access.missing.assign"
 
     @api.model
     def document_quick_access_models(self):
-        models = self.env['document.quick.access.rule'].search([]).mapped(
-            'model_id'
-        )
+        models = self.env["document.quick.access.rule"].search([]).mapped("model_id")
         res = []
         for model in models:
             res.append((model.model, model.name))
         return res
 
     object_id = fields.Reference(
-        selection=lambda r: r.document_quick_access_models(),
-        required=True,
+        selection=lambda r: r.document_quick_access_models(), required=True
     )
     missing_document_id = fields.Many2one(
-        'document.quick.access.missing',
-        required=True,
+        "document.quick.access.missing", required=True
     )
 
     @api.multi
     def doit(self):
         self.ensure_one()
-        self.missing_document_id.assign_model(
-            self.object_id._name, self.object_id.id)
+        self.missing_document_id.assign_model(self.object_id._name, self.object_id.id)
         return True
