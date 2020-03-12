8047facf414d01320fa256f80f588e03be7d6ebb
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-12 12:28:07 +0100

    [MIG] document_quick_access_folder_auto_classification: Migration to 13.0

--- document_quick_access_folder_auto_classification/__manifest__.py
+++ document_quick_access_folder_auto_classification/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Document Quick Access Folder Auto Classification",
     "summary": """
         Auto classification of Documents after reading a QR""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-ux",
