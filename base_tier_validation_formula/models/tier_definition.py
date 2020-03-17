d45992d6b00d7008627114b5c18a9651f4bafec7
ps-tubtim <pimolnats@ecosoft.co.th>
2020-03-17 09:15:19 +0700

    [12.0][fix] base_tier_validation_formula: Add default

--- base_tier_validation_formula/models/tier_definition.py
+++ base_tier_validation_formula/models/tier_definition.py
@@ -12,7 +12,7 @@ class TierDefinition(models.Model):
         help="Write Python code that defines when this tier confirmation "
         "will be needed. The result of executing the expresion must be "
         "a boolean.",
-        default="""# Available locals:\n#  - rec: current record""",
+        default="""# Available locals:\n#  - rec: current record\nTrue""",
     )
     definition_type = fields.Selection(
         selection_add=[("formula", "Formula"), ("domain_formula", "Domain & Formula")]
@@ -23,17 +23,16 @@ class TierDefinition(models.Model):
         "The result of executing the expression must be a res.users "
         "recordset.",
         default="# Available locals:\n#  - rec: current record\n"
-        "#  - Expects a recordset of res.users",
+        "#  - Expects a recordset of res.users\nrec.env.user",
     )
     review_type = fields.Selection(selection_add=[("expression", "Python Expression")])
 
     @api.onchange("review_type")
     def onchange_review_type(self):
         super(TierDefinition, self).onchange_review_type()
-        self.reviewer_expression = False
-        if self.review_type == "expression":
-            self.reviewer_expression = (
-                "# Available locals:\n"
-                "#  - rec: current record\n"
-                "#  - Expects a recordset of res.users"
-            )
+        self.reviewer_expression = (
+            "# Available locals:\n"
+            "#  - rec: current record\n"
+            "#  - Expects a recordset of res.users\n"
+            "rec.env.user"
+        )
