01fbcbf8bed454ef7e751d3aae39e844f70b6be2
Saran440 <saranl@ecosoft.co.th>
2020-03-25 15:14:27 +0700

    [12.0][FIX] base_tier_validation : convert datetime timezone

--- base_tier_validation/__manifest__.py
+++ base_tier_validation/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Base Tier Validation",
     "summary": "Implement a validation process based on tiers.",
-    "version": "13.0.1.2.2",
+    "version": "13.0.1.2.3",
     "development_status": "Mature",
     "maintainers": ["LoisRForgeFlow"],
     "category": "Tools",
