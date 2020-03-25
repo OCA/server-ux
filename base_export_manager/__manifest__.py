65d330c00204e8ee6126b58d9631030762b1c3dd
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-03-25 19:23:18 +0100

    [IMP] base_export_manager: black, isort, prettier

--- base_export_manager/__manifest__.py
+++ base_export_manager/__manifest__.py
@@ -5,29 +5,25 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': "Manage model export profiles",
-    'category': 'Personalization',
-    'version': '12.0.1.0.0',
-    'depends': [
-        'web',
+    "name": "Manage model export profiles",
+    "category": "Personalization",
+    "version": "13.0.1.0.0",
+    "depends": ["web"],
+    "data": [
+        "views/assets.xml",
+        "views/ir_exports.xml",
+        "views/ir_model.xml",
+        "views/ir_model_access.xml",
+        "views/res_groups.xml",
     ],
-    'data': [
-        'views/assets.xml',
-        'views/ir_exports.xml',
-        'views/ir_model.xml',
-        'views/ir_model_access.xml',
-        'views/res_groups.xml',
-    ],
-    'qweb': [
-        "static/src/xml/base.xml",
-    ],
-    'author': 'Tecnativa, '
-              'LasLabs, '
-              'Ursa Information Systems, '
-              'brain-tec AG, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/server-ux',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
+    "qweb": ["static/src/xml/base.xml"],
+    "author": "Tecnativa, "
+    "LasLabs, "
+    "Ursa Information Systems, "
+    "brain-tec AG, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-ux",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
 }
