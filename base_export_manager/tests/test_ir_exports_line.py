1d802da5438f0a2cc6f7ec81c4d09d287f76bebf
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-03-25 19:32:54 +0100

    [MIG] base_export_manager: Migration to 13.0

--- base_export_manager/tests/test_ir_exports_line.py
+++ base_export_manager/tests/test_ir_exports_line.py
@@ -23,41 +23,42 @@ class TestIrExportsLineCase(TransactionCase):
             [("name", "=", "name"), ("model_id", "=", self.partner_model.id)]
         )
 
-    def test_check_name(self):
+    def _record_create(self, field_name):
         m_ir_exports_line = self.env["ir.exports.line"]
-        m_ir_exports_line.create({"name": "name", "export_id": self.export.id})
+        virt_line = m_ir_exports_line.new(
+            {"name": field_name, "export_id": self.export.id}
+        )
+        virt_line._inverse_name()
+        return m_ir_exports_line.create(virt_line._convert_to_write(virt_line._cache))
+
+    def test_check_name(self):
+        self._record_create("name")
         with self.assertRaises(ValidationError):
-            m_ir_exports_line.create({"name": "name", "export_id": self.export.id})
+            self._record_create("name")
         with self.assertRaises(ValidationError):
-            m_ir_exports_line.create(
-                {"name": "bad_error_name", "export_id": self.export.id}
-            )
+            self._record_create("bad_error_name")
 
     def test_get_label_string(self):
-        m_ir_exports_line = self.env["ir.exports.line"]
-        export_line = m_ir_exports_line.create(
-            {"name": "parent_id/name", "export_id": self.export.id}
-        )
+        export_line = self._record_create("parent_id/name")
         self.assertEqual(
             export_line.with_context(lang="en_US").label,
             "Related Company/Name (parent_id/name)",
         )
         with self.assertRaises(ValidationError):
-            m_ir_exports_line.create({"name": "", "export_id": self.export.id})
+            self._record_create("")
 
     def test_model_default_by_context(self):
         """Fields inherit the model_id by context."""
-        line = (
-            self.env["ir.exports.line"]
-            .with_context(default_model1_id=self.export.model_id.id)
-            .create({"name": "name", "export_id": self.export.id})
-        )
+        m_ir_exports_line = self.env["ir.exports.line"]
+        virt_line = m_ir_exports_line.new({"name": "name", "export_id": self.export.id})
+        virt_line._inverse_name()
+        line = m_ir_exports_line.with_context(
+            default_model1_id=self.export.model_id.id
+        ).create(virt_line._convert_to_write(virt_line._cache))
         self.assertEqual(line.model1_id, self.export.model_id)
 
     def test_inverse_name(self):
-        line = self.env["ir.exports.line"].create(
-            {"export_id": self.export.id, "name": "parent_id/parent_id/parent_id/name"}
-        )
+        line = self._record_create("parent_id/parent_id/parent_id/name")
         self.assertEqual(line.model1_id, self.partner_model)
         self.assertEqual(line.model2_id, self.partner_model)
         self.assertEqual(line.field1_id, self.field_parent_id)
@@ -78,11 +79,7 @@ class TestIrExportsLineCase(TransactionCase):
         self.assertEqual(line.name, "parent_id/parent_id/parent_id/name")
 
     def test_write_name_same_root(self):
-        self.env["ir.exports.line"].create(
-            {"export_id": self.export.id, "name": "parent_id"}
-        )
-        line = self.env["ir.exports.line"].create(
-            {"export_id": self.export.id, "name": "name"}
-        )
+        self._record_create("parent_id")
+        line = self._record_create("name")
         # This should end without errors
         line.name = "parent_id/name"
