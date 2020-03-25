ad36e012afbd94d1d9ae3870784da8ed48e521ea
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-03-25 19:32:54 +0100

    [MIG] base_export_manager: Migration to 13.0

--- base_export_manager/models/ir_model_access.py
+++ base_export_manager/models/ir_model_access.py
@@ -8,4 +8,6 @@ from odoo import fields, models
 class IrModelAccess(models.Model):
     _inherit = "ir.model.access"
 
+    # WARN: This can't be used in 'check()'
+    # See https://github.com/odoo/odoo/blob/0b6a2569920b6584652c39b3465998649fe305b4/odoo/addons/base/models/ir_model.py#L1496  # noqa: B950
     perm_export = fields.Boolean("Export Access", default=True)
