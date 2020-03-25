c1db02fb8c29cf51ffaec0cede8a6026f7dee114
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-03-25 19:32:54 +0100

    [MIG] base_export_manager: Migration to 13.0

--- base_export_manager/models/ir_exports.py
+++ base_export_manager/models/ir_exports.py
@@ -9,51 +9,37 @@ class IrExports(models.Model):
     _inherit = "ir.exports"
 
     name = fields.Char(required=True)
-    resource = fields.Char(
-        required=False, readonly=True, help="Model's technical name."
-    )
+    resource = fields.Char(readonly=True, help="Model's technical name.")
     model_id = fields.Many2one(
         "ir.model",
         "Model",
         store=True,
-        domain=[("transient", "=", False)],
-        compute="_compute_model_id",
         inverse="_inverse_model_id",
+        compute="_compute_model_id",
+        domain=[("transient", "=", False)],
         help="Database model to export.",
     )
 
-    @api.multi
-    @api.depends("resource")
-    def _compute_model_id(self):
-        """Get the model from the resource."""
-        for s in self:
-            s.model_id = self._get_model_id(s.resource)
-
-    @api.multi
-    @api.onchange("model_id")
-    def _inverse_model_id(self):
-        """Get the resource from the model."""
-        for s in self:
-            s.resource = s.model_id.model
-
-    @api.multi
     @api.onchange("resource")
     def _onchange_resource(self):
-        """Void fields if model is changed in a view."""
-        for s in self:
-            s.export_fields = False
+        self.ensure_one()
+        self.export_fields = [(5, 0, 0)]
 
-    @api.model
-    def _get_model_id(self, resource):
-        """Return a model object from its technical name.
+    @api.depends("model_id")
+    def _inverse_model_id(self):
+        """ Gets resource from model """
+        for record in self:
+            record.resource = self.model_id.model
 
-        :param str resource:
-            Technical name of the model, like ``ir.model``.
-        """
-        return self.env["ir.model"].search([("model", "=", resource)])
+    @api.depends("resource")
+    def _compute_model_id(self):
+        """ Gets resource from model """
+        IrModel = self.env["ir.model"]
+        for record in self:
+            record.model_id = IrModel._get(record.resource)
 
-    @api.model
-    def create(self, vals):
+    @api.model_create_multi
+    def create(self, vals_list):
         """Check required values when creating the record.
 
         Odoo's export dialog populates ``resource``, while this module's new
@@ -61,6 +47,7 @@ class IrExports(models.Model):
         trigger the methods that fill up the other, so this should fail if
         one is missing.
         """
-        if not any(f in vals for f in {"model_id", "resource"}):
-            raise ValidationError(_("You must supply a model or resource."))
-        return super(IrExports, self).create(vals)
+        for vals in vals_list:
+            if not any(f in vals for f in ("model_id", "resource")):
+                raise ValidationError(_("You must supply a model or resource."))
+        return super().create(vals_list)
