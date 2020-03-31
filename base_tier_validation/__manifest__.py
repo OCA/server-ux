d8063e035b1d1a5adaf910344dd686e6b2a55102
Enric Tobella <etobella@creublanca.es>
2020-03-31 13:34:04 +0200

    [FIX] tier_validation: Security issues with record permissions

--- base_tier_validation/__manifest__.py
+++ base_tier_validation/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Base Tier Validation",
     "summary": "Implement a validation process based on tiers.",
-    "version": "13.0.1.0.2",
+    "version": "13.0.1.1.0",
     "development_status": "Mature",
     "maintainers": ["LoisRForgeFlow"],
     "category": "Tools",
