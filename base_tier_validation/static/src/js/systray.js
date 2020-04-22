52a7fccfa1fb96ff005601cb83c9f30884fb539e
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2020-04-22 11:31:34 +0200

    pre-commit update

--- base_tier_validation/static/src/js/systray.js
+++ base_tier_validation/static/src/js/systray.js
@@ -1,29 +1,27 @@
-odoo.define('tier_validation.systray', function (require) {
+odoo.define("tier_validation.systray", function(require) {
     "use strict";
 
-    var core = require('web.core');
-    var session = require('web.session');
-    var SystrayMenu = require('web.SystrayMenu');
-    var Widget = require('web.Widget');
+    var core = require("web.core");
+    var session = require("web.session");
+    var SystrayMenu = require("web.SystrayMenu");
+    var Widget = require("web.Widget");
 
     var QWeb = core.qweb;
 
     var ReviewMenu = Widget.extend({
-        template:'tier.validation.ReviewMenu',
+        template: "tier.validation.ReviewMenu",
         events: {
             "show.bs.dropdown": "_onReviewMenuShow",
-            'click .o_mail_activity_action': '_onReviewActionClick',
-            'click .o_mail_preview': '_onReviewFilterClick',
+            "click .o_mail_activity_action": "_onReviewActionClick",
+            "click .o_mail_preview": "_onReviewFilterClick",
         },
-        start: function () {
-            this.$reviews_preview = this.$('.o_mail_systray_dropdown_items');
+        start: function() {
+            this.$reviews_preview = this.$(".o_mail_systray_dropdown_items");
             this._updateReviewPreview();
-            var channel = 'base.tier.validation';
-            this.call('bus_service', 'addChannel', channel);
-            this.call('bus_service', 'startPolling');
-            this.call(
-                'bus_service', 'onNotification', this, this._updateReviewPreview
-            );
+            var channel = "base.tier.validation";
+            this.call("bus_service", "addChannel", channel);
+            this.call("bus_service", "startPolling");
+            this.call("bus_service", "onNotification", this, this._updateReviewPreview);
             return this._super();
         },
 
@@ -34,23 +32,29 @@ odoo.define('tier_validation.systray', function (require) {
          * @private
          * @returns {integer}
          */
-        _getReviewData: function () {
+        _getReviewData: function() {
             var self = this;
 
-            return self._rpc({
-                model: 'res.users',
-                method: 'review_user_count',
-                kwargs: {
-                    context: session.user_context,
-                },
-            }).then(function (data) {
-                self.reviews = data;
-                self.reviewCounter = _.reduce(data, function (total_count, p_data) {
-                    return total_count + p_data.pending_count;
-                }, 0);
-                self.$('.o_notification_counter').text(self.reviewCounter);
-                self.$el.toggleClass('o_no_notification', !self.reviewCounter);
-            });
+            return self
+                ._rpc({
+                    model: "res.users",
+                    method: "review_user_count",
+                    kwargs: {
+                        context: session.user_context,
+                    },
+                })
+                .then(function(data) {
+                    self.reviews = data;
+                    self.reviewCounter = _.reduce(
+                        data,
+                        function(total_count, p_data) {
+                            return total_count + p_data.pending_count;
+                        },
+                        0
+                    );
+                    self.$(".o_notification_counter").text(self.reviewCounter);
+                    self.$el.toggleClass("o_no_notification", !self.reviewCounter);
+                });
         },
 
         /**
@@ -59,10 +63,10 @@ odoo.define('tier_validation.systray', function (require) {
          * @param {String} model
          * @returns {integer}
          */
-        _getReviewModelViewID: function (model) {
+        _getReviewModelViewID: function(model) {
             return this._rpc({
                 model: model,
-                method: 'get_activity_view_id',
+                method: "get_activity_view_id",
             });
         },
 
@@ -70,13 +74,14 @@ odoo.define('tier_validation.systray', function (require) {
          * Update(render) activity system tray view on activity updation.
          * @private
          */
-        _updateReviewPreview: function () {
+        _updateReviewPreview: function() {
             var self = this;
-            self._getReviewData().then(function () {
-                self.$reviews_preview.html(QWeb.render(
-                    'tier.validation.ReviewMenuPreview', {
-                        reviews : self.reviews,
-                    }));
+            self._getReviewData().then(function() {
+                self.$reviews_preview.html(
+                    QWeb.render("tier.validation.ReviewMenuPreview", {
+                        reviews: self.reviews,
+                    })
+                );
             });
         },
 
@@ -88,16 +93,16 @@ odoo.define('tier_validation.systray', function (require) {
          * @param {Boolean} [data.activity_deleted] when activity deleted
          * @param {Boolean} [data.activity_created] when activity created
          */
-        _updateCounter: function (data) {
+        _updateCounter: function(data) {
             if (data) {
                 if (data.review_created) {
-                    this.reviewCounter ++;
+                    this.reviewCounter++;
                 }
                 if (data.review_deleted && this.reviewCounter > 0) {
-                    this.reviewCounter --;
+                    this.reviewCounter--;
                 }
-                this.$('.o_notification_counter').text(this.reviewCounter);
-                this.$el.toggleClass('o_no_notification', !this.reviewCounter);
+                this.$(".o_notification_counter").text(this.reviewCounter);
+                this.$el.toggleClass("o_no_notification", !this.reviewCounter);
             }
         },
 
@@ -110,9 +115,9 @@ odoo.define('tier_validation.systray', function (require) {
          * @private
          * @param {MouseEvent} ev
          */
-        _onReviewActionClick: function (ev) {
+        _onReviewActionClick: function(ev) {
             ev.stopPropagation();
-            var actionXmlid = $(ev.currentTarget).data('action_xmlid');
+            var actionXmlid = $(ev.currentTarget).data("action_xmlid");
             this.do_action(actionXmlid);
         },
 
@@ -121,24 +126,30 @@ odoo.define('tier_validation.systray', function (require) {
          * @private
          * @param {MouseEvent} event
          */
-        _onReviewFilterClick: function (event) {
+        _onReviewFilterClick: function(event) {
             // Fetch the data from the button otherwise fetch the ones from the
             // parent (.o_tier_channel_preview).
-            var data = _.extend({}, $(
-                event.currentTarget).data(), $(event.target).data());
+            var data = _.extend(
+                {},
+                $(event.currentTarget).data(),
+                $(event.target).data()
+            );
             var context = {};
             this.do_action({
-                type: 'ir.actions.act_window',
+                type: "ir.actions.act_window",
                 name: data.model_name,
                 res_model: data.res_model,
-                views: [[false, 'list'], [false, 'form']],
+                views: [
+                    [false, "list"],
+                    [false, "form"],
+                ],
                 search_view_id: [false],
                 domain: [
-                    ['review_ids.reviewer_ids', '=', session.uid],
-                    ['review_ids.status', '=', 'pending'],
-                    ['review_ids.can_review', '=', true],
+                    ["review_ids.reviewer_ids", "=", session.uid],
+                    ["review_ids.status", "=", "pending"],
+                    ["review_ids.can_review", "=", true],
                 ],
-                context:context,
+                context: context,
             });
         },
 
@@ -147,10 +158,9 @@ odoo.define('tier_validation.systray', function (require) {
          * @private
          * @param {MouseEvent} event
          */
-        _onReviewMenuShow: function () {
+        _onReviewMenuShow: function() {
             this._updateReviewPreview();
         },
-
     });
 
     SystrayMenu.Items.push(ReviewMenu);
