54b7fec695b152bcaa613bbfbc5ff909f65cce48
Tonow-c2c <thomas.nowicki@camptocamp.com>
2020-05-14 15:01:52 +0200

    [IMP] base_user_locale: black, isort, prettier

--- base_user_locale/models/res_users.py
+++ base_user_locale/models/res_users.py
@@ -5,25 +5,25 @@ from odoo import fields, models
 
 
 class ResUsers(models.Model):
-    _inherit = 'res.users'
+    _inherit = "res.users"
 
     date_format = fields.Char(
-        string='Date Format',
-        help='See Settings > Translations > Languages for details',
+        string="Date Format",
+        help="See Settings > Translations > Languages for details",
     )
     time_format = fields.Char(
-        string='Time Format',
-        help='See Settings > Translations > Languages for details',
+        string="Time Format",
+        help="See Settings > Translations > Languages for details",
     )
     week_start = fields.Selection(
-        string='Week Start',
+        string="Week Start",
         selection=[
-            ('1', 'Monday'),
-            ('2', 'Tuesday'),
-            ('3', 'Wednesday'),
-            ('4', 'Thursday'),
-            ('5', 'Friday'),
-            ('6', 'Saturday'),
-            ('7', 'Sunday'),
+            ("1", "Monday"),
+            ("2", "Tuesday"),
+            ("3", "Wednesday"),
+            ("4", "Thursday"),
+            ("5", "Friday"),
+            ("6", "Saturday"),
+            ("7", "Sunday"),
         ],  # NOTE: Numbers as strings otherwise UI works poorly
     )
--- base_user_locale/tests/test_base_user_locale.py
+++ base_user_locale/tests/test_base_user_locale.py
@@ -7,124 +7,99 @@ from ..controllers.web_client import WebClient
 
 
 class TestBaseUserLocale(TransactionCase):
-
     def setUp(self):
         super().setUp()
 
-        self.ResCompany = self.env['res.company']
-        self.ResUsers = self.env['res.users']
-        self.CalendarEvent = self.env['calendar.event']
+        self.ResCompany = self.env["res.company"]
+        self.ResUsers = self.env["res.users"]
+        self.CalendarEvent = self.env["calendar.event"]
 
     def test_date_format(self):
-        company = self.ResCompany.create({
-            'name': 'Company',
-        })
-        user = self.ResUsers.with_context(
-            no_reset_password=True
-        ).create({
-            'name': 'User',
-            'login': 'user',
-            'email': 'user@example.com',
-            'company_id': company.id,
-            'company_ids': [(4, company.id)],
-            'lang': 'en_US',
-        })
+        company = self.ResCompany.create({"name": "Company",})
+        user = self.ResUsers.with_context(no_reset_password=True).create(
+            {
+                "name": "User",
+                "login": "user",
+                "email": "user@example.com",
+                "company_id": company.id,
+                "company_ids": [(4, company.id)],
+                "lang": "en_US",
+            }
+        )
 
         self.assertEqual(
-            self.CalendarEvent.sudo(user)._get_date_formats()[0],
-            '%B-%d-%Y'
+            self.CalendarEvent.sudo(user)._get_date_formats()[0], "%B-%d-%Y"
         )
         lang_parameters = WebClient().get_user_lang_parameters(user)
         self.assertEqual(lang_parameters, {})
 
-        company.date_format = '%d %b %Y'
+        company.date_format = "%d %b %Y"
         self.assertEqual(
-            self.CalendarEvent.sudo(user)._get_date_formats()[0],
-            '%d %b %Y'
+            self.CalendarEvent.sudo(user)._get_date_formats()[0], "%d %b %Y"
         )
         lang_parameters = WebClient().get_user_lang_parameters(user)
-        self.assertEqual(lang_parameters, {
-            'date_format': '%d %b %Y',
-        })
+        self.assertEqual(lang_parameters, {"date_format": "%d %b %Y",})
 
-        user.date_format = '%d/%b/%Y'
+        user.date_format = "%d/%b/%Y"
         self.assertEqual(
-            self.CalendarEvent.sudo(user)._get_date_formats()[0],
-            '%d/%b/%Y'
+            self.CalendarEvent.sudo(user)._get_date_formats()[0], "%d/%b/%Y"
         )
         lang_parameters = WebClient().get_user_lang_parameters(user)
-        self.assertEqual(lang_parameters, {
-            'date_format': '%d/%b/%Y',
-        })
+        self.assertEqual(lang_parameters, {"date_format": "%d/%b/%Y",})
 
     def test_time_format(self):
-        company = self.ResCompany.create({
-            'name': 'Company',
-        })
-        user = self.ResUsers.with_context(
-            no_reset_password=True
-        ).create({
-            'name': 'User',
-            'login': 'user',
-            'email': 'user@example.com',
-            'company_id': company.id,
-            'company_ids': [(4, company.id)],
-            'lang': 'en_US',
-        })
+        company = self.ResCompany.create({"name": "Company",})
+        user = self.ResUsers.with_context(no_reset_password=True).create(
+            {
+                "name": "User",
+                "login": "user",
+                "email": "user@example.com",
+                "company_id": company.id,
+                "company_ids": [(4, company.id)],
+                "lang": "en_US",
+            }
+        )
 
         self.assertEqual(
-            self.CalendarEvent.sudo(user)._get_date_formats()[1],
-            '%I-%M %p'
+            self.CalendarEvent.sudo(user)._get_date_formats()[1], "%I-%M %p"
         )
         lang_parameters = WebClient().get_user_lang_parameters(user)
         self.assertEqual(lang_parameters, {})
 
-        company.time_format = '%H.%M.%S'
+        company.time_format = "%H.%M.%S"
         self.assertEqual(
-            self.CalendarEvent.sudo(user)._get_date_formats()[1],
-            '%H.%M.%S'
+            self.CalendarEvent.sudo(user)._get_date_formats()[1], "%H.%M.%S"
         )
         lang_parameters = WebClient().get_user_lang_parameters(user)
-        self.assertEqual(lang_parameters, {
-            'time_format': '%H.%M.%S',
-        })
+        self.assertEqual(lang_parameters, {"time_format": "%H.%M.%S",})
 
-        user.time_format = '%I:%M%p'
+        user.time_format = "%I:%M%p"
         self.assertEqual(
-            self.CalendarEvent.sudo(user)._get_date_formats()[1],
-            '%I:%M%p'
+            self.CalendarEvent.sudo(user)._get_date_formats()[1], "%I:%M%p"
         )
         lang_parameters = WebClient().get_user_lang_parameters(user)
-        self.assertEqual(lang_parameters, {
-            'time_format': '%I:%M%p',
-        })
+        self.assertEqual(lang_parameters, {"time_format": "%I:%M%p",})
 
     def test_week_start(self):
-        company = self.ResCompany.create({
-            'name': 'Company',
-        })
-        user = self.ResUsers.with_context(
-            no_reset_password=True
-        ).create({
-            'name': 'User',
-            'login': 'user',
-            'email': 'user@example.com',
-            'company_id': company.id,
-            'company_ids': [(4, company.id)],
-            'lang': 'en_US',
-        })
+        company = self.ResCompany.create({"name": "Company",})
+        user = self.ResUsers.with_context(no_reset_password=True).create(
+            {
+                "name": "User",
+                "login": "user",
+                "email": "user@example.com",
+                "company_id": company.id,
+                "company_ids": [(4, company.id)],
+                "lang": "en_US",
+            }
+        )
 
         lang_parameters = WebClient().get_user_lang_parameters(user)
         self.assertEqual(lang_parameters, {})
 
-        company.week_start = '4'
+        company.week_start = "4"
         lang_parameters = WebClient().get_user_lang_parameters(user)
-        self.assertEqual(lang_parameters, {
-            'week_start': 4,
-        })
+        self.assertEqual(lang_parameters, {"week_start": 4,})
 
-        user.week_start = '2'
+        user.week_start = "2"
         lang_parameters = WebClient().get_user_lang_parameters(user)
-        self.assertEqual(lang_parameters, {
-            'week_start': 2,
-        })
+        self.assertEqual(lang_parameters, {"week_start": 2,})
