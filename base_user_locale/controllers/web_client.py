8f5c0202eb427b3d003697d35cad7ff3b93a6fd0
Tonow-c2c <thomas.nowicki@camptocamp.com>
2020-05-14 15:23:23 +0200

    [MIG] base_user_locale: Migration to 13.0

--- base_user_locale/controllers/web_client.py
+++ base_user_locale/controllers/web_client.py
@@ -1,6 +1,8 @@
 # Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+import json
+
 from odoo.http import request, route
 
 from odoo.addons.web.controllers.main import WebClient, ensure_db
@@ -11,19 +13,15 @@ class WebClient(WebClient):
         res = {}
         date_format = user.date_format or user.company_id.date_format
         if date_format:
-            res.update(
-                {"date_format": date_format,}
-            )
+            res.update({"date_format": date_format})
         time_format = user.time_format or user.company_id.time_format
         if time_format:
-            res.update(
-                {"time_format": time_format,}
-            )
+            res.update({"time_format": time_format})
         week_start = user.week_start or user.company_id.week_start
         if week_start:
             res.update(
-                {"week_start": int(week_start),}  # NOTE: WebClient needs int
-            )
+                {"week_start": int(week_start)}
+            )  # NOTE: WebClient needs int
         return res
 
     @route()
@@ -31,6 +29,11 @@ class WebClient(WebClient):
         res = super().translations(mods, lang)
         if "uid" in request.session:
             ensure_db()
-            user = request.env["res.users"].sudo().browse(request.session["uid"])
-            res["lang_parameters"].update(self.get_user_lang_parameters(user))
+            user = request.env["res.users"].sudo().browse(
+                request.session["uid"]
+            )
+            json_data = res.get_data()
+            data = json.loads(json_data)
+            data['lang_parameters'].update(self.get_user_lang_parameters(user))
+            res.set_data(json.dumps(data))
         return res
