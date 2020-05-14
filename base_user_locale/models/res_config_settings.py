9d6a45ed51f1dbe050899f20fb9d53596d259fa6
Tonow-c2c <thomas.nowicki@camptocamp.com>
2020-05-14 15:01:52 +0200

    [IMP] base_user_locale: black, isort, prettier

--- base_user_locale/models/res_config_settings.py
+++ base_user_locale/models/res_config_settings.py
@@ -1,21 +1,12 @@
 # Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
-    date_format = fields.Char(
-        related='company_id.date_format',
-        readonly=False,
-    )
-    time_format = fields.Char(
-        related='company_id.time_format',
-        readonly=False,
-    )
-    week_start = fields.Selection(
-        related='company_id.week_start',
-        readonly=False,
-    )
+    date_format = fields.Char(related="company_id.date_format", readonly=False,)
+    time_format = fields.Char(related="company_id.time_format", readonly=False,)
+    week_start = fields.Selection(related="company_id.week_start", readonly=False,)
--- base_user_locale/static/src/js/session.js
+++ base_user_locale/static/src/js/session.js
@@ -1,10 +1,10 @@
 // Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
 // License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-odoo.define('base_user_locale.session', function(require) {
-    'use strict';
+odoo.define("base_user_locale.session", function(require) {
+    "use strict";
 
-    var core = require('web.core');
-    var Session = require('web.Session');
+    var core = require("web.core");
+    var Session = require("web.Session");
 
     var _t = core._t;
 
@@ -12,16 +12,16 @@ odoo.define('base_user_locale.session', function(require) {
         /**
          * @override
          */
-        load_modules: function () {
+        load_modules: function() {
             var self = this;
-            return this._super.apply(this, arguments).then(function () {
+            return this._super.apply(this, arguments).then(function() {
                 return self.configure_locale();
             });
         },
         /**
          * See https://github.com/odoo/odoo/pull/36532
          */
-        configure_locale: function () {
+        configure_locale: function() {
             moment.updateLocale(moment.locale(), {
                 week: {
                     // Moment uses index 0 for Sunday but Odoo stores it as 7:
