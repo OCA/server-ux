ae124b0d348ae260cb181bf7eb31f3a37384566e
Jesus Ramoneda <chusamo@gmail.com>
2020-05-19 10:41:02 +0200

    [IMP] base_search_custom_field_filter: black, isort, prettier

--- base_search_custom_field_filter/models/ir_ui_custom_field_filter.py
+++ base_search_custom_field_filter/models/ir_ui_custom_field_filter.py
@@ -13,27 +13,30 @@ class IrUiCustomFilter(models.Model):
     sequence = fields.Integer()
     model_id = fields.Many2one(comodel_name="ir.model", required=True)
     model_name = fields.Char(
-        related="model_id.model", store=True, readonly=True, index=True,
+        related="model_id.model",
+        store=True,
+        readonly=True,
+        index=True,
         string="Model name",
     )
     name = fields.Char(required=True, translate=True)
     expression = fields.Char(required=True)
     position_after = fields.Char(
         help="Optional field name for putting the filter after that one. "
-             "If empty or not found, it will be put at the end.",
+        "If empty or not found, it will be put at the end.",
     )
 
     def _get_related_field(self):
         """Determine the chain of fields."""
         self.ensure_one()
-        related = self.expression.split('.')
+        related = self.expression.split(".")
         target = self.env[self.model_name]
         for name in related:
             field = target._fields[name]
             target = target[name]
         return field
 
-    @api.constrains('model_id', 'expression')
+    @api.constrains("model_id", "expression")
     def _check_expression(self):
         for record in self:
             try:
