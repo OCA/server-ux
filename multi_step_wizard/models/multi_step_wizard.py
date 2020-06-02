07ef6fdc45ea51a23ba5ed870292ca865d564385
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-06-02 11:08:42 +0200

    [12.0][IMP] multi_step_wizard: Go Back

--- multi_step_wizard/models/multi_step_wizard.py
+++ multi_step_wizard/models/multi_step_wizard.py
@@ -18,10 +18,14 @@ class MultiStepWizard(models.AbstractModel):
     "state_exit_X" where X is the name of the state. Each
     of these method must set the next state in self.state.
 
+    For each state but start, there may be a method named
+    "state_previous_X" where X is the name of the state. Each
+    of these method must set the next state in self.state.
+
     The final state has no related method because the view
     should only display a button to close the wizard.
 
-    open_next and _reopen_self should not need to be
+    open_next, open_previous and _reopen_self should not need to be
     overidden, but _selection_state and state_exit_start
     likely will need to.
 
@@ -34,12 +38,29 @@ class MultiStepWizard(models.AbstractModel):
         selection="_selection_state", default="start", required=True
     )
 
+    allow_back = fields.Boolean(compute="_compute_allow_back")
+
+    def _compute_allow_back(self):
+        for record in self:
+            record.allow_back = getattr(
+                record, "state_previous_%s" % record.state, False
+            )
+
     @api.model
     def _selection_state(self):
         return [("start", "Start"), ("final", "Final")]
 
     def open_next(self):
         state_method = getattr(self, "state_exit_{}".format(self.state), None)
+        if state_method is None:
+            raise NotImplementedError(
+                "No method defined for state {}".format(self.state)
+            )
+        state_method()
+        return self._reopen_self()
+
+    def open_previous(self):
+        state_method = getattr(self, "state_previous_{}".format(self.state), None)
         if state_method is None:
             raise NotImplementedError(
                 "No method defined for state {}".format(self.state)
