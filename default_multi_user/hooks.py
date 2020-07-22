c3a6d8559a7ccc6cb905944692925fa1c9368b43
Lois Rilo <lois.rilo@forgeflow.com>
2020-07-22 16:12:50 +0200

    [IMP] default_multi_user: black, isort, prettier

--- default_multi_user/hooks.py
+++ default_multi_user/hooks.py
@@ -1,24 +1,22 @@
 # Copyright 2020 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.addons.base.models.ir_default import IrDefault
+import json
+
 from odoo import api, tools
 
-import json
+from odoo.addons.base.models.ir_default import IrDefault
 
 
 def post_load_hook():
-
     @api.model
-    @tools.ormcache('self.env.uid', 'model_name', 'condition')
+    @tools.ormcache("self.env.uid", "model_name", "condition")
     def new_get_model_defaults(self, model_name, condition=False):
-        if not hasattr(self, '_get_model_defaults_query_and_params'):
-            return self.get_model_defaults_original(
-                model_name, condition=condition)
+        if not hasattr(self, "_get_model_defaults_query_and_params"):
+            return self.get_model_defaults_original(model_name, condition=condition)
         cr = self.env.cr
         # START OF CHANGES
-        query, params = self._get_model_defaults_query_and_params(
-            model_name, condition)
+        query, params = self._get_model_defaults_query_and_params(model_name, condition)
         # END OF CHANGES
         cr.execute(query, params)
         result = {}
@@ -28,7 +26,7 @@ def post_load_hook():
                 result[row[0]] = json.loads(row[1])
         return result
 
-    if not hasattr(IrDefault, 'get_model_defaults_original'):
+    if not hasattr(IrDefault, "get_model_defaults_original"):
         IrDefault.get_model_defaults_original = IrDefault.get_model_defaults
 
     IrDefault._patch_method("get_model_defaults", new_get_model_defaults)
