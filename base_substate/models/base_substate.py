82c398eca755e6ef6a89caba48db1466f61fed9f
Kitti U <kittiu@gmail.com>
2020-08-30 10:45:46 +0700

    [IMP] base_substate: black, isort, prettier

--- base_substate/models/base_substate.py
+++ base_substate/models/base_substate.py
@@ -15,15 +15,18 @@ class BaseSubstateType(models.Model):
      - model: sale.order
      - target_state_field: state
     """
+
     _name = "base.substate.type"
-    _description = 'Base Substate Type'
-    _order = 'name asc, model asc'
+    _description = "Base Substate Type"
+    _order = "name asc, model asc"
 
     name = fields.Char(required=True, translate=True)
-    model = fields.Selection(selection=[], string='Apply on', required=True)
+    model = fields.Selection(selection=[], string="Apply on", required=True)
     target_state_field = fields.Char(
-        required=True, help='Technical target state field name.'
-        ' Ex for sale order "state" for other "status" ... ')
+        required=True,
+        help="Technical target state field name."
+        ' Ex for sale order "state" for other "status" ... ',
+    )
 
 
 class TargetStateValue(models.Model):
@@ -32,27 +35,32 @@ class TargetStateValue(models.Model):
     Data in this model should be created by import as technical data
     in specific module ex : sale_subsatate
     """
+
     _name = "target.state.value"
-    _description = 'Target State Value'
-    _order = 'name asc'
+    _description = "Target State Value"
+    _order = "name asc"
 
     name = fields.Char(
-        'Target state Name',
+        "Target state Name",
         required=True,
         translate=True,
-        help='Target state translateble name.\n'
-        'Ex: for sale order "Quotation", "Sale order", "Locked"...')
+        help="Target state translateble name.\n"
+        'Ex: for sale order "Quotation", "Sale order", "Locked"...',
+    )
     base_substate_type_id = fields.Many2one(
-        'base.substate.type',
-        string='Substate Type',
-        ondelete='restrict',
+        "base.substate.type", string="Substate Type", ondelete="restrict",
     )
     target_state_value = fields.Char(
-        required=True, help='Technical target state value.\n'
-        'Ex: for sale order "draft", "sale", "done", ...')
-    model = fields.Selection(related='base_substate_type_id.model',
-                             store=True, readonly=True,
-                             help="Model for technical use")
+        required=True,
+        help="Technical target state value.\n"
+        'Ex: for sale order "draft", "sale", "done", ...',
+    )
+    model = fields.Selection(
+        related="base_substate_type_id.model",
+        store=True,
+        readonly=True,
+        help="Model for technical use",
+    )
 
 
 class BaseSubstate(models.Model):
@@ -64,26 +72,29 @@ class BaseSubstate(models.Model):
     "Won" and "Lost".
     We can also send mail when the susbstate is reached.
     """
-    _name = "base.substate"
-    _description = 'Base Substate'
-    _order = 'active desc, sequence asc'
 
-    name = fields.Char('Substate Name', required=True, translate=True)
+    _name = "base.substate"
+    _description = "Base Substate"
+    _order = "active desc, sequence asc"
+
+    name = fields.Char("Substate Name", required=True, translate=True)
     description = fields.Text(translate=True)
     sequence = fields.Integer(
-        index=True,
-        help="Gives the sequence order when applying the default substate",
+        index=True, help="Gives the sequence order when applying the default substate",
     )
     target_state_value_id = fields.Many2one(
-        'target.state.value',
-        string='Target State Value',
-        ondelete='restrict')
+        "target.state.value", string="Target State Value", ondelete="restrict"
+    )
     active = fields.Boolean(default=True)
     mail_template_id = fields.Many2one(
-        'mail.template',
-        string='Email Template',
+        "mail.template",
+        string="Email Template",
         help="If set, an email will be sent to the partner "
-        "when the object reaches this substate.")
-    model = fields.Selection(related='target_state_value_id.model',
-                             store=True, readonly=True,
-                             help="Model for technical use")
+        "when the object reaches this substate.",
+    )
+    model = fields.Selection(
+        related="target_state_value_id.model",
+        store=True,
+        readonly=True,
+        help="Model for technical use",
+    )
--- base_substate/models/base_substate_mixin.py
+++ base_substate/models/base_substate_mixin.py
@@ -1,20 +1,23 @@
 # Copyright 2020 Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class BaseSubstateMixin(models.AbstractModel):
     _name = "base.substate.mixin"
-    _description = 'BaseSubstate Mixin'
+    _description = "BaseSubstate Mixin"
 
     def _get_default_substate_id(self, state_val=False):
         """ Gives default substate_id """
         search_domain = self._get_default_substate_domain(state_val)
         # perform search, return the first found
-        return self.env['base.substate'].search(search_domain,
-                                                order='sequence', limit=1).id
+        return (
+            self.env["base.substate"]
+            .search(search_domain, order="sequence", limit=1)
+            .id
+        )
 
     def _get_default_substate_domain(self, state_val=False):
         """ Override this method
@@ -27,53 +30,52 @@ class BaseSubstateMixin(models.AbstractModel):
         if self and not state_val and state_field in self._fields:
             state_val = self[state_field]
 
-        domain = [('target_state_value_id.target_state_value', '=', state_val)]
-        domain += [('target_state_value_id.base_substate_type_id',
-                    '=', substate_type.id)]
+        domain = [("target_state_value_id.target_state_value", "=", state_val)]
+        domain += [
+            ("target_state_value_id.base_substate_type_id", "=", substate_type.id)
+        ]
         return domain
 
     def _get_default_state_value(self,):
         """ Override this method
             to change state_value
             """
-        return 'draft'
+        return "draft"
 
     def _get_substate_type(self,):
         """ Override this method
             to change substate_type (get by xml id for example)
         """
-        return self.env['base.substate.type'].search(
-            [('model', '=', self._name)], limit=1)
+        return self.env["base.substate.type"].search(
+            [("model", "=", self._name)], limit=1
+        )
 
     substate_id = fields.Many2one(
-        'base.substate',
-        string='Sub State',
-        ondelete='restrict',
+        "base.substate",
+        string="Sub State",
+        ondelete="restrict",
         default=lambda self: self._get_default_substate_id(),
-        track_visibility='onchange',
+        track_visibility="onchange",
         index=True,
-        domain=lambda self: [
-            ('model',
-             '=',
-             self._name)],
-        copy=False)
+        domain=lambda self: [("model", "=", self._name)],
+        copy=False,
+    )
 
-    @api.constrains('substate_id')
+    @api.constrains("substate_id")
     def check_substate_id_consistency(self):
         for mixin_obj in self:
-            if (mixin_obj.substate_id and
-                    mixin_obj.substate_id.model != self._name):
+            if mixin_obj.substate_id and mixin_obj.substate_id.model != self._name:
                 raise ValidationError(
-                    _("This substate is not define for this object but for %s"
-                      ) %
-                    mixin_obj.substate_id.model)
+                    _("This substate is not define for this object but for %s")
+                    % mixin_obj.substate_id.model
+                )
 
     def _update_before_write_create(self, values):
         substate_type = self._get_substate_type()
         state_field = substate_type.target_state_field
-        if values.get(state_field) and not values.get('substate_id'):
+        if values.get(state_field) and not values.get("substate_id"):
             state_val = values.get(state_field)
-            values['substate_id'] = self._get_default_substate_id(state_val)
+            values["substate_id"] = self._get_default_substate_id(state_val)
         return values
 
     @api.multi
