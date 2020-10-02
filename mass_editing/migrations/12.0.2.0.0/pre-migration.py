06945cfdcc6f28154159eff72bb928a98c711828
Simone Orsi <simahawk@gmail.com>
2020-10-02 14:47:30 +0200

    mass_editing: fwd port v10 -> v12 mig scripts

--- mass_editing/migrations/12.0.2.0.0/pre-migration.py
+++ mass_editing/migrations/12.0.2.0.0/pre-migration.py
@@ -1,41 +1,13 @@
 # Copyright (C) 2019 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# Copyright 2020 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from openupgradelib import openupgrade
 
-def migrate(cr, version):
-    if not version:
+
+def migrate(cr, installed_version):
+    if not installed_version:
         return
 
-    # First remove the obsolete constraint created by the existence of the
-    # remove M2M field 'field_ids' between mass.object and ir.model.fields
-    cr.execute(
-        """
-        DELETE FROM ir_model_relation
-        WHERE name = 'mass_field_rel';
-        """
-    )
-    # Rename table for consistency reason
-    cr.execute(
-        """
-        ALTER TABLE mass_object
-        RENAME TO mass_editing;
-    """
-    )
-    # Create and Compute new required field
-    cr.execute(
-        """
-        ALTER TABLE mass_editing
-        ADD COLUMN action_name varchar;
-    """
-    )
-    cr.execute(
-        """
-        UPDATE mass_editing
-        SET action_name = name;
-    """
-    )
-    cr.execute(
-        """
-        alter sequence mass_object_id_seq rename to mass_editing_id_seq;"""
-    )
+    openupgrade.rename_tables(cr, [('mass_object', 'mass_editing')])
