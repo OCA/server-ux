2042944b2127655430035894e2f57d8c6e349ce3
Patrick Tombez <patrick.tombez@camptocamp.com>
2020-10-30 16:41:33 +0100

    Fix linting

--- base_technical_features/models/res_users.py
+++ base_technical_features/models/res_users.py
@@ -22,8 +22,8 @@ class ResUsers(models.Model):
 
     @api.depends("groups_id")
     def _compute_show_technical_features(self):
-        """ Only display the technical features checkbox in the user
-        preferences if the user has access to them """
+        """Only display the technical features checkbox in the user
+        preferences if the user has access to them"""
         users = self.env.ref("base.group_no_one").users
         for user in self:
             user.show_technical_features = user in users
@@ -36,8 +36,8 @@ class ResUsers(models.Model):
             user.technical_features = user in users
 
     def _inverse_technical_features(self):
-        """ Map boolean field value to group membership, but checking
-        access """
+        """Map boolean field value to group membership, but checking
+        access"""
         group = self.env.ref("base_technical_features.group_technical_features")
         for user in self:
             if self.env.ref("base.group_no_one") not in user.groups_id:
@@ -51,5 +51,8 @@ class ResUsers(models.Model):
 
     def __init__(self, pool, cr):
         super().__init__(pool, cr)
-        type(self).SELF_READABLE_FIELDS += ["technical_features", "show_technical_features"]
+        type(self).SELF_READABLE_FIELDS += [
+            "technical_features",
+            "show_technical_features",
+        ]
         type(self).SELF_WRITEABLE_FIELDS.append("technical_features")
