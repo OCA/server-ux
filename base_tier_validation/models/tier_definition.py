24cf282a1e127ecc182ec82fb3f1efc8147bf93e
Lois Rilo <lois.rilo@forgeflow.com>
2020-11-10 10:28:41 +0100

    [13.0][IMP] base_tier_validation: make module multi-company compatible.

--- base_tier_validation/models/tier_definition.py
+++ base_tier_validation/models/tier_definition.py
@@ -50,7 +50,7 @@ class TierDefinition(models.Model):
     company_id = fields.Many2one(
         comodel_name="res.company",
         string="Company",
-        default=lambda self: self.env.user.company_id,
+        default=lambda self: self.env.company,
     )
     notify_on_create = fields.Boolean(
         string="Notify Reviewers on Creation",
