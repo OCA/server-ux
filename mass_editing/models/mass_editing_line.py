ef3cc6b41014454d14647e57cdd568fd29a0182a
Víctor Martínez <victor.martinez@tecnativa.com>
2020-11-17 10:05:47 +0100

    [MIG] mass_editing: Add apply domain option in lines to fix some many2one fields with default domain (company_id for example)

--- mass_editing/models/mass_editing_line.py
+++ mass_editing/models/mass_editing_line.py
@@ -38,15 +38,19 @@ class MassEditingLine(models.Model):
         "'many2many_tags', 'selection'",
     )
 
+    apply_domain = fields.Boolean(
+        default=False,
+        string="Apply domain",
+        help="Apply default domain related to field"
+    )
+
     @api.depends("field_id")
     def _compute_widget_option(self):
         # this function propose selection, depending on the field
         for line in self.filtered("field_id"):
             field = line.field_id
             line.widget_option = False
-            if field.ttype == "many2one":
-                line.widget_option = "selection"
-            elif field.ttype == "many2many":
+            if field.ttype == "many2many":
                 line.widget_option = "many2many_tags"
             elif field.ttype == "Binary":
                 if "image" in field.name or "logo" in field.name:
