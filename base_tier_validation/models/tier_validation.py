fe8b4bcf4d90a6fd87f20e9e6238791887168d04
tanghulu0608 <39181819@qq.com>
2020-12-27 17:12:00 +0800

    [FIX] some terms can not be translated.

--- base_tier_validation/models/tier_validation.py
+++ base_tier_validation/models/tier_validation.py
@@ -238,7 +238,7 @@ class TierValidation(models.AbstractModel):
         )
         if has_comment:
             comment = has_comment.mapped("comment")[0]
-            return _("A review was accepted. (%s)" % comment)
+            return _("A review was accepted. (%s)") % comment
         return _("A review was accepted")
 
     def _add_comment(self, validate_reject, reviews):
@@ -283,9 +283,8 @@ class TierValidation(models.AbstractModel):
         )
         if has_comment:
             comment = has_comment.mapped("comment")[0]
-            return _(
-                "A review was rejected by {}. ({})".format(self.env.user.name, comment)
-            )
+            return _("A review was rejected by {}. ({})").format(self.env.user.name, comment)
+
         return _("A review was rejected by %s.") % (self.env.user.name)
 
     def _notify_rejected_review(self):
