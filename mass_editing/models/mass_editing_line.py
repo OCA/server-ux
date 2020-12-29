6c55a8f626d5ab134a7f70635b15dc5d6fb32c3a
Ivàn Todorovich <ivan.todorovich@gmail.com>
2020-12-29 10:53:35 -0300

    [MIG] mass_editing: Migration to 14.0
    Refactor
    --------
    
    Deprecate dependency on mass_operation_abstract
    Rely on ir.actions.server instead.
    
    Now, a mass.editing operation is nothing more than a
    ir.actions.server of type "mass_edit".
    
    [IMP] Add onchange and constraints for consistency with field and models
    
    [IMP] Ignore MAGIC_FIELDS and don't allow to create fields from mass.editing form

--- mass_editing/models/mass_editing_line.py
+++ mass_editing/models/mass_editing_line.py
@@ -1,40 +1,48 @@
 # Copyright (C) 2019 - Today: GRAP (http://www.grap.coop)
+# Copyright (C) 2020 Iván Todorovich (https://twitter.com/ivantodorovich)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+MAGIC_FIELDS = models.MAGIC_COLUMNS + [models.BaseModel.CONCURRENCY_CHECK_FIELD]
 
 
 class MassEditingLine(models.Model):
     _name = "mass.editing.line"
-    _order = "sequence,field_id"
     _description = "Mass Editing Line"
+    _order = "sequence, field_id"
 
     sequence = fields.Integer()
-
-    mass_editing_id = fields.Many2one(comodel_name="mass.editing")
-
+    server_action_id = fields.Many2one(
+        "ir.actions.server",
+        string="Server Action",
+        ondelete="cascade",
+        required=True,
+    )
     model_id = fields.Many2one(
-        comodel_name="ir.model", related="mass_editing_id.model_id"
+        "ir.model",
+        related="server_action_id.model_id",
     )
-
     field_id = fields.Many2one(
-        comodel_name="ir.model.fields",
+        "ir.model.fields",
         string="Field",
-        domain="["
-        "('name', '!=', '__last_update'),"
-        "('ttype', 'not in', ['reference', 'function']),"
-        "('model_id', '=', model_id)"
-        "]",
+        domain="""
+            [
+                ("name", "not in", %s),
+                ("ttype", "not in", ["reference", "function"]),
+                ("model_id", "=", model_id),
+            ]
+        """
+        % str(MAGIC_FIELDS),
+        ondelete="cascade",
+        required=True,
     )
-
     widget_option = fields.Char(
         string="Widget Option",
-        store=True,
-        readonly=False,
-        help="Add widget text that will be used"
-        " to display the field in the wizard. Example :\n"
-        "'many2many_tags', 'selection'",
+        help="Add widget text that will be used to display the field in the wizard.\n"
+        "Example: 'many2many_tags', 'selection', 'image'",
     )
     apply_domain = fields.Boolean(
         default=False,
@@ -42,10 +50,21 @@ class MassEditingLine(models.Model):
         help="Apply default domain related to field",
     )
 
+    @api.constrains("server_action_id", "field_id")
+    def _check_field_model(self):
+        """ Check that all fields belong to the action model """
+        if any(rec.field_id.model_id != rec.server_action_id.model_id for rec in self):
+            raise ValidationError(
+                _("Mass edit fields should belong to the server action model.")
+            )
+
     @api.onchange("field_id")
     def _onchange_field_id(self):
-        if self.field_id and self.field_id.ttype == "many2many":
-            self.widget_option = "many2many_tags"
-        elif self.field_id and self.field_id.ttype == "Binary":
-            if "image" in self.field_id.name or "logo" in self.field_id.name:
-                self.widget_option = "image"
+        for rec in self:
+            if rec.field_id.ttype == "many2many":
+                rec.widget_option = "many2many_tags"
+            elif rec.field_id.ttype == "binary":
+                if "image" in rec.field_id.name or "logo" in rec.field_id.name:
+                    rec.widget_option = "image"
+            else:
+                rec.widget_option = False
--- mass_editing/readme/CONFIGURE.rst
+++ mass_editing/readme/CONFIGURE.rst
@@ -1,17 +1,16 @@
-* Go to *Settings / Mass Editing / Mass Editing* and configure the object and fields for Mass Editing.
+* Go to *Settings / Technical / Actions / Server Actions* and configure the object and fields for Mass Editing.
 
 * Select the object and add the fields of that object on which you want to apply mass editing.
 
 .. image:: ../static/description/mass_editing_form.png
    :width: 70%
 
-* *Add Action*: As shown in figure click on *Add Sidebar Button* to add mass editing option in *Action* option in action.
+* *Add Action*: Click on *Create Contextual Action* to add mass editing in *Action* menu.
 
 
 **Options**
 
-* You can limit the mass editing by a domain.
+* You can add an extra message that will be displayed in the wizard.
 
-* you can limit the access to the option to a given group.
-
-* you can add an extra message that will be displayed in the wizard.
+* This module plays nicely with `server_action_domain`, allowing you to limit
+  the mass editing action with a domain.
