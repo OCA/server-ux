11cb6be8157fcd4db9c3adef3b0571947b8a783e
Ivàn Todorovich <ivan.todorovich@gmail.com>
2020-12-29 15:45:59 -0300

    [ADD] server_action_domain : Module to filter server action records

---
+++ server_action_domain/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Iván Todorovich (https://twitter.com/ivantodorovich)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Server Action Domain",
+    "version": "14.0.1.0.0",
+    "summary": "Apply a domain filter before executing server actions on records",
+    "author": "Iván Todorovich, Odoo Community Association (OCA)",
+    "category": "Tools",
+    "website": "https://github.com/OCA/server-ux",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": [
+        "views/ir_actions_server.xml",
+    ],
+}
