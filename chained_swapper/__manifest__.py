aabdd4b004a3ccbb236c715d35f2a56ee8cf27cc
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2021-01-18 13:35:18 -0500

    [MIG] chained_swapper: Migration to 13.0

--- chained_swapper/__manifest__.py
+++ chained_swapper/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Chained Swapper",
     "summary": "Chained Swapper",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "category": "Tools",
     "website": "https://github.com/OCA/server-ux",
@@ -14,6 +14,6 @@
         "views/chained_swapper_views.xml",
         "wizard/chained_swapper_wizard_views.xml",
     ],
-    "demo": ["demo/chained_swapper_demo.xml",],
+    "demo": ["demo/chained_swapper_demo.xml"],
     "uninstall_hook": "uninstall_hook",
 }
