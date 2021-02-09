555f6dc222348511b99b65e46c972f1f0a40a583
Kitti U <kittiu@gmail.com>
2021-02-09 10:14:43 +0700

    [14.0][IMP] base_tier_validation, add 'Todo by' column in review table

--- base_tier_validation/models/tier_review.py
+++ base_tier_validation/models/tier_review.py
@@ -1,7 +1,7 @@
 # Copyright 2017-19 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
 
 
 class TierReview(models.Model):
@@ -36,6 +36,7 @@ class TierReview(models.Model):
         store=True,
     )
     sequence = fields.Integer(string="Tier")
+    todo_by = fields.Char(compute="_compute_todo_by", store=True)
     done_by = fields.Many2one(comodel_name="res.users")
     requested_by = fields.Many2one(comodel_name="res.users")
     reviewed_date = fields.Datetime(string="Validation Date")
@@ -76,6 +77,21 @@ class TierReview(models.Model):
     def _compute_reviewer_ids(self):
         for rec in self:
             rec.reviewer_ids = rec._get_reviewers()
+
+    @api.depends("reviewer_ids")
+    def _compute_todo_by(self):
+        """ Show by group or by abbrev list of names """
+        num_show = 3  # Max number of users to display
+        for rec in self:
+            todo_by = False
+            if rec.reviewer_group_id:
+                todo_by = _("Group %s") % rec.reviewer_group_id.name
+            else:
+                todo_by = ", ".join(rec.reviewer_ids[:num_show].mapped("display_name"))
+                num_users = len(rec.reviewer_ids)
+                if num_users > num_show:
+                    todo_by = "{} (and {} more)".format(todo_by, num_users - num_show)
+            rec.todo_by = todo_by
 
     def _get_reviewers(self):
         return self.reviewer_id + self.reviewer_group_id.users
