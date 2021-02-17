33874c373df619e2b32bfbec7158eb9c1ca97a0b
Murtaza Mithaiwala <mmithaiwala@opensourceintegrators.com>
2021-02-17 15:42:00 +0530

    [IMP] Code After pre-commit.

--- base_import_security_group/__manifest__.py
+++ base_import_security_group/__manifest__.py
@@ -4,24 +4,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Optional CSV import',
-    'version': '13.0.1.0.0',
-    'category': 'Server tools',
-    'summary': 'Group-based permissions for importing CSV files',
-    'license': 'AGPL-3',
-    'author': 'Odoo Community Association (OCA), '
-              'Alejandro Santana <alejandrosantana@anubia.es>, '
-              'Onestein'
-              'Open Source Integrators, (OSI)',
-    'maintainer': 'Odoo Community Association (OCA)',
-    'website': 'http://odoo-community.org',
-    'depends': [
-        'web',
-        'base_import',
+    "name": "Optional CSV import",
+    "version": "13.0.1.0.0",
+    "category": "Server tools",
+    "summary": "Group-based permissions for importing CSV files",
+    "license": "AGPL-3",
+    "author": "Odoo Community Association (OCA), "
+    "Alejandro Santana <alejandrosantana@anubia.es>, "
+    "Onestein"
+    "Open Source Integrators, (OSI)",
+    "maintainer": "Odoo Community Association (OCA)",
+    "website": "http://odoo-community.org",
+    "depends": ["web", "base_import",],
+    "data": [
+        "security/base_import_security_group_security.xml",
+        "views/base_import.xml",
     ],
-    'data': [
-        'security/base_import_security_group_security.xml',
-        'views/base_import.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
