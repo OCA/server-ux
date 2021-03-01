30a968bb6ea7c1506ad657562a6bc7e9e1168d62
schhatbar <sunanda.chhatbar@initos.com>
2021-03-01 19:01:34 +0530

    [IMP] default_multi_user: black, isort, prettier

--- default_multi_user/__manifest__.py
+++ default_multi_user/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Default Multi User",
     "summary": "Allows to share user-defined defaults among several users.",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.1",
     "development_status": "Beta",
     "maintainers": ["LoisRForgeFlow"],
     "category": "Tools",
