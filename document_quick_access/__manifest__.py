7f2305a9ea048dada3237febadf83e94309b2a83
fshah <foram.shah@initos.com>
2021-03-02 11:53:42 +0530

    [IMP] document_quick_access: isort, black, prettier.

--- document_quick_access/__manifest__.py
+++ document_quick_access/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Document Quick Access",
     "summary": """
         Document quick access""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-ux",
