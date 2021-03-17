04d39e5b3079650a423027fbcba0e6df4c489480
Pablo Fuentes <pablo@studio73.es>
2021-03-17 18:49:00 +0100

    [FIX] date_range: Refactor component with Owl framework

--- date_range/static/src/js/date_range.js
+++ date_range/static/src/js/date_range.js
@@ -1,132 +1,88 @@
-/* Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
- * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
-odoo.define("date_range.search_filters", function (require) {
+/* Copyright 2016 ACSONE SA/NV (<https://acsone.eu>)
+ * Copyright 2021 Studio73 - Pablo Fuentes (https://www.studio73.es)
+ * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
+
+odoo.define("date_range.CustomFilterItem", function (require) {
     "use strict";
 
-    var filters = require("web.search_filters");
-    var rpc = require("web.rpc");
-    var framework = require("web.framework");
+    const {_lt} = require("web.core");
+    const session = require("web.session");
+    const {FIELD_OPERATORS, FIELD_TYPES} = require("web.searchUtils");
+    const CustomFilterItem = require("web.CustomFilterItem");
 
-    filters.ExtendedSearchProposition.include({
-        select_field: function (field) {
-            this._super.apply(this, arguments);
-            this.is_date_range_selected = false;
-            this.is_date = field.type === "date" || field.type === "datetime";
-            this.$value = this.$el.find(
-                ".searchview_extended_prop_value, .o_searchview_extended_prop_value"
-            );
-            if (this.is_date) {
-                this._rpc({
-                    model: "date.range.type",
-                    method: "search_read",
-                    fields: ["name"],
-                    context: this.context,
-                }).then(this.proxy("add_date_range_types_operator"));
-            }
-        },
-
-        add_date_range_types_operator: function (date_range_types) {
-            var self = this;
-            _.each(date_range_types, function (drt) {
-                var el = self.$el.find(
-                    ".searchview_extended_prop_op, .o_searchview_extended_prop_op"
-                );
-                $("<option>", {value: "drt_" + drt.id})
-                    .text(_("in ") + drt.name)
-                    .appendTo(el);
-            });
-        },
-
-        operator_changed: function (e) {
-            var val = $(e.target).val();
-            this.is_date_range_selected = val.startsWith("drt_");
-            if (this.is_date_range_selected) {
-                var type_id = val.replace("drt_", "");
-                this.date_range_type_operator_selected(type_id);
-                return;
-            }
-            this._super.apply(this, arguments);
-        },
-
-        date_range_type_operator_selected: function (type_id) {
-            this.$value.empty().show();
-            this._rpc({
-                model: "date.range",
-                method: "search_read",
-                fields: ["name", "date_start", "date_end"],
-                context: this.context,
-                domain: [["type_id", "=", parseInt(type_id, 10)]],
-            }).then(this.proxy("on_range_type_selected"));
-        },
-
-        on_range_type_selected: function (date_range_values) {
-            this.value = new filters.ExtendedSearchProposition.DateRange(
-                this,
-                this.value.field,
-                date_range_values
-            );
-            var self = this;
-            this.value.appendTo(this.$value).then(function () {
-                if (!self.$el.hasClass("o_filter_condition")) {
-                    self.$value.find(".date-range-select").addClass("form-control");
+    CustomFilterItem.patch(
+        "date_range.CustomFilterItem",
+        (T) =>
+            class extends T {
+                constructor() {
+                    super(...arguments);
+                    this._computeDateRangeOperators();
                 }
-                self.value.on_range_selected();
-            });
-        },
 
-        get_filter: function () {
-            var res = this._super.apply(this, arguments);
-            if (this.is_date_range_selected) {
-                // In case of date.range, the domain is provided by the server and we don't
-                // Want to put nest the returned value into an array.
-                res.attrs.domain = this.value.domain;
+                async _computeDateRangeOperators() {
+                    this.OPERATORS = Object.assign({}, FIELD_OPERATORS);
+                    this.OPERATORS.date = [...FIELD_OPERATORS.date];
+                    this.OPERATORS.datetime = [...FIELD_OPERATORS.datetime];
+                    this.date_ranges = {};
+                    const result = await this.rpc({
+                        model: "date.range",
+                        method: "search_read",
+                        fields: ["name", "type_id", "date_start", "date_end"],
+                        context: session.user_context,
+                    });
+                    result.forEach((range) => {
+                        const range_type = range.type_id[0];
+                        if (this.date_ranges[range_type] === undefined) {
+                            const r = {
+                                symbol: "between",
+                                description: `${_lt("in")} ${range.type_id[1]}`,
+                                date_range: true,
+                                date_range_type: range_type,
+                            };
+                            this.OPERATORS.date.push(r);
+                            this.OPERATORS.datetime.push(r);
+                            this.date_ranges[range_type] = [];
+                        }
+                        this.date_ranges[range_type].push(range);
+                    });
+                }
+
+                _setDefaultValue(condition) {
+                    const type = this.fields[condition.field].type;
+                    const operator = this.OPERATORS[FIELD_TYPES[type]][
+                        condition.operator
+                    ];
+                    if (operator.date_range) {
+                        const default_range = this.date_ranges[
+                            operator.date_range_type
+                        ][0];
+                        const d_start = moment(`${default_range.date_start} 00:00:00`);
+                        const d_end = moment(`${default_range.date_end} 23:59:59`);
+                        condition.value = [d_start, d_end];
+                    } else {
+                        super._setDefaultValue(...arguments);
+                    }
+                }
+
+                _onValueInput(condition, ev) {
+                    const type = this.fields[condition.field].type;
+                    const operator = this.OPERATORS[FIELD_TYPES[type]][
+                        condition.operator
+                    ];
+                    if (operator.date_range) {
+                        this.date_ranges[operator.date_range_type].forEach((range) => {
+                            if (range.id === ev.target.value) {
+                                const d_start = moment(`${range.date_start} 00:00:00`);
+                                const d_end = moment(`${range.date_end} 23:59:59`);
+                                condition.value = [d_start, d_end];
+                            }
+                        });
+                    } else {
+                        super._onValueInput(...arguments);
+                    }
+                }
             }
-            return res;
-        },
-    });
+    );
 
-    filters.ExtendedSearchProposition.DateRange = filters.Field.extend({
-        template: "SearchView.extended_search.dateRange.selection",
-        events: {
-            change: "on_range_selected",
-        },
-
-        init: function (parent, field, date_range_values) {
-            this._super(parent, field);
-            this.date_range_values = date_range_values;
-        },
-
-        toString: function () {
-            var select = this.$el[0];
-            var option = select.options[select.selectedIndex];
-            return option.label || option.text;
-        },
-
-        get_value: function () {
-            return parseInt(this.$el.val(), 10);
-        },
-
-        on_range_selected: function () {
-            var self = this;
-            self.domain = "";
-            framework.blockUI();
-            return rpc
-                .query({
-                    args: [this.get_value()],
-                    kwargs: {
-                        field_name: this.field.name,
-                    },
-                    model: "date.range",
-                    method: "get_domain",
-                })
-                .then(function (domain) {
-                    framework.unblockUI();
-                    self.domain = domain;
-                });
-        },
-
-        get_domain: function () {
-            return this.domain;
-        },
-    });
+    return CustomFilterItem;
 });
