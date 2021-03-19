0f0eea163af2399254be9fc7e2012972528651f4
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-03-19 20:30:01 +0100

    [FIX] base_export_manager: Don't return on invalid field, but break
    If not, we are not assigning the label value for the record and we get
    a missing cache value error.

--- base_export_manager/__manifest__.py
+++ base_export_manager/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Manage model export profiles",
     "category": "Personalization",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "depends": ["web"],
     "data": [
         "views/assets.xml",
