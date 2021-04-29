f90b043a6c05c35faed1da42039b737b2d416615
mreficent <miquel.raich@eficent.com>
2021-04-29 19:15:16 +0200

    [MIG] base_user_locale: migration continuation

--- base_user_locale/models/calendar_event.py
+++ base_user_locale/models/calendar_event.py
@@ -2,7 +2,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, models
-from odoo.tools import pycompat
 
 
 class CalendarEvent(models.Model):
@@ -11,10 +10,10 @@ class CalendarEvent(models.Model):
     @api.model
     def _get_date_formats(self):
         format_date, format_time = super()._get_date_formats()
-        date_format = self.env.user.date_format or self.env.user.company_id.date_format
+        date_format = self.env.user.date_format or self.env.company.date_format
         if date_format:
-            format_date = pycompat.to_native(date_format)
-        time_format = self.env.user.time_format or self.env.user.company_id.time_format
+            format_date = str(date_format)
+        time_format = self.env.user.time_format or self.env.company.time_format
         if time_format:
-            format_time = pycompat.to_native(time_format)
+            format_time = str(time_format)
         return (format_date, format_time)
