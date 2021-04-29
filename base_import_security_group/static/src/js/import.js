17bee67776a320c67979ef290be18698c744840e
Víctor Martínez <victor.martinez@tecnativa.com>
2021-04-29 16:03:51 +0200

    [IMP] base_import_security_group: black, isort, prettier

--- base_import_security_group/static/src/js/import.js
+++ base_import_security_group/static/src/js/import.js
@@ -1,42 +1,39 @@
-odoo.define('web.ListImport', function (require) {
+odoo.define("web.ListImport", function(require) {
     "use strict";
 
-    var KanbanController = require('web.KanbanController');
-    var KanbanView = require('web.KanbanView');
-    var ListController = require('web.ListController');
-    var ListView = require('web.ListView');
-    var session = require('web.session');
+    var KanbanController = require("web.KanbanController");
+    var KanbanView = require("web.KanbanView");
+    var ListController = require("web.ListController");
+    var ListView = require("web.ListView");
+    var session = require("web.session");
 
     var ImportViewMixin = {
+        init: function(viewInfo, params) {
+            var self = this;
+            var result = self._super.apply(self, arguments);
+            var base_group = "base_import_security_group.group_import_csv";
 
-    init: function (viewInfo, params) {
-
-        var self = this
-        var result = self._super.apply(self, arguments);
-        var base_group = 'base_import_security_group.group_import_csv';
-
-        session.user_has_group(base_group).then(function (result){
-            var importEnabled = false
-            if (result){
-                importEnabled =  true;
-            }
-            self.controllerParams.importEnabled = importEnabled;
+            session.user_has_group(base_group).then(function(result) {
+                var importEnabled = false;
+                if (result) {
+                    importEnabled = true;
+                }
+                self.controllerParams.importEnabled = importEnabled;
             });
         },
-
     };
 
     ListView.include({
-    init: function () {
-        this._super.apply(this, arguments);
-        ImportViewMixin.init.apply(this, arguments);
+        init: function() {
+            this._super.apply(this, arguments);
+            ImportViewMixin.init.apply(this, arguments);
         },
     });
 
     KanbanView.include({
-    init: function () {
-        this._super.apply(this, arguments);
-        ImportViewMixin.init.apply(this, arguments);
+        init: function() {
+            this._super.apply(this, arguments);
+            ImportViewMixin.init.apply(this, arguments);
         },
     });
 });
