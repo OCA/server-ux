b24d8aa194f6b4bc54c3d759e4d7e5ed853060f3
Víctor Martínez <victor.martinez@tecnativa.com>
2021-04-29 16:03:51 +0200

    base_import_security_group: black, isort, prettier

--- base_import_security_group/tests/test_base_import_security_group.py
+++ base_import_security_group/tests/test_base_import_security_group.py
@@ -9,9 +9,9 @@ from odoo.tools import mute_logger
 class TestImportSecurityGroup(common.HttpCase):
     def setUp(self):
         super().setUp()
-        self.Access = self.env['ir.model.access']
-        self.user_test = self.env.ref('base.user_demo')
-        self.user_admin = self.env.ref('base.user_admin')
+        self.Access = self.env["ir.model.access"]
+        self.user_test = self.env.ref("base.user_demo")
+        self.user_admin = self.env.ref("base.user_admin")
 
     def has_button_import(self, falsify=False, user=None):
         """
@@ -25,46 +25,51 @@ class TestImportSecurityGroup(common.HttpCase):
                 console.log('error');
             };
         }, 2000);
-        """ % ('!' if falsify else '')
-        action = self.env.ref('base.action_partner_category_form').id
-        link = '/web#action=%s' % action
+        """ % (
+            "!" if falsify else ""
+        )
+        action = self.env.ref("base.action_partner_category_form").id
+        link = "/web#action=%s" % action
         self.browser_js(
-            link, code, 'Boolean($(".o_list_button_add"))',
-            login=user.login)
+            link, code, 'Boolean($(".o_list_button_add"))', login=user.login
+        )
 
     def test_01_load(self):
         """ Admin user can import data, but the demo user cannot """
         fields = (
-            'id',
-            'name',
-            'perm_read',
-            'perm_write',
-            'perm_create',
-            'perm_unlink',
+            "id",
+            "name",
+            "perm_read",
+            "perm_write",
+            "perm_create",
+            "perm_unlink",
         )
         data = [
-            ('access_res_users_test', 'res.users test', '1', '0', '0', '0'),
-            ('access_res_users_test2', 'res.users test2', '1', '1', '1', '1'),
+            ("access_res_users_test", "res.users test", "1", "0", "0", "0"),
+            ("access_res_users_test2", "res.users test2", "1", "1", "1", "1"),
         ]
         self.has_button_import(user=self.user_admin)
-        with mute_logger('odoo.sql_db'):
+        with mute_logger("odoo.sql_db"):
             res = self.Access.sudo(self.user_admin).load(fields, data)
 
-        self.assertEqual(res['ids'], False)
-        self.assertEqual(len(res['messages']), 2)
+        self.assertEqual(res["ids"], False)
+        self.assertEqual(len(res["messages"]), 2)
         self.assertEqual(
-            res['messages'][0]['message'],
-            "Missing required value for the field 'Object' (model_id)")
+            res["messages"][0]["message"],
+            "Missing required value for the field 'Object' (model_id)",
+        )
         self.assertEqual(
-            res['messages'][1]['message'],
-            "Missing required value for the field 'Object' (model_id)")
+            res["messages"][1]["message"],
+            "Missing required value for the field 'Object' (model_id)",
+        )
 
         self.has_button_import(falsify=True, user=self.user_test)
         res2 = self.Access.sudo(self.user_test).load(fields, data)
 
-        self.assertEqual(res2['ids'], None)
-        self.assertEqual(len(res2['messages']), 1)
+        self.assertEqual(res2["ids"], None)
+        self.assertEqual(len(res2["messages"]), 1)
         self.assertEqual(
-            res2['messages'][0]['message'],
-            'User (ID: %s) is not allowed to import data in '
-            'model ir.model.access.' % self.user_test.id)
+            res2["messages"][0]["message"],
+            "User (ID: %s) is not allowed to import data in "
+            "model ir.model.access." % self.user_test.id,
+        )
