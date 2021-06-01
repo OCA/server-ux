dc245abf24b83c9c657bc37af898059c8a8f7b07
Stefan Rijnhart <stefan@opener.am>
2021-06-01 16:15:02 +0200

    [RFR] base_import_security_group: pass ability through the web session
    * Patch existing class instead of duplicating it, for better compatibility
      with other modules enhancing the same class.
    * To circumvent problems executing async calls in a non-widget class,
      pass the user's import permission through the web session.
    * Adapt tests to consult he Favorite dropdown in the 14.0 GUI.
    * In the tour, don't target the Users menu which is the default menu of the
      Settings menu when no other module is installed, and in that case clicking
      the menu in the tour is a void action that makes the tests fail. Target the
      Companies menu item instead.

--- base_import_security_group/models/__init__.py
+++ base_import_security_group/models/__init__.py
@@ -2,4 +2,5 @@
 # Copyright 2017 Onestein (http://www.onestein.eu)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import models
+from . import base
+from . import ir_http
