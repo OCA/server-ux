06af8e0002d58efde23dca4b93d65ffe159f1382
Jordi Ballester <jordi.ballester@forgeflow.com>
2021-07-05 11:40:23 +0200

    [FIX] base_tier_validation: avoid singleton error

--- base_tier_validation/__manifest__.py
+++ base_tier_validation/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Base Tier Validation",
     "summary": "Implement a validation process based on tiers.",
-    "version": "14.0.2.3.0",
+    "version": "14.0.2.4.0",
     "development_status": "Mature",
     "maintainers": ["LoisRForgeFlow"],
     "category": "Tools",
