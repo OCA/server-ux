220b72f75b1c130bf569569a65e265f3b00f3233
João Marques <joao.marques@tecnativa.com>
2021-07-09 07:41:44 +0100

    [FIX] date_range: Add comment to missing dependency for odoo_test_helper

--- date_range/__manifest__.py
+++ date_range/__manifest__.py
@@ -9,7 +9,10 @@
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
-    "depends": ["web"],
+    "depends": [
+        "web",
+    ],
+    # odoo_test_helper is needed for the tests
     "data": [
         "security/ir.model.access.csv",
         "security/date_range_security.xml",
