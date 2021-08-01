d5214856c4f250c95e182d20dd41f94e1c212928
Alexis de Lattre <alexis.delattre@akretion.com>
2021-08-01 11:13:43 +0200

    Add module date_range_account

---
+++ date_range_account/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Date Range Account",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "summary": "Add Date Range menu entry in Invoicing app",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/server-ux",
+    "depends": ["account", "date_range"],
+    "data": [
+        "views/date_range.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
