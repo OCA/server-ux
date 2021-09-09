f741c4e0023f36c89d2db0b9fc8c5d4a54adc97d
hkapatel <helly.kapatel@initos.com>
2021-09-09 17:16:00 +0530

    [MIG] document_quick_access_folder_auto_classification: Migration to 14.0

--- document_quick_access_folder_auto_classification/__manifest__.py
+++ document_quick_access_folder_auto_classification/__manifest__.py
@@ -5,11 +5,12 @@
     "name": "Document Quick Access Folder Auto Classification",
     "summary": """
         Auto classification of Documents after reading a QR""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-ux",
-    "depends": ["document_quick_access", "queue_job"],
+    "depends": ["document_quick_access", "edi_storage_oca"],
+    "external_dependencies": {"python": ["pyzbar"]},
     "data": [
         "security/security.xml",
         "security/ir.model.access.csv",
--- document_quick_access_folder_auto_classification/readme/CONTRIBUTORS.rst
+++ document_quick_access_folder_auto_classification/readme/CONTRIBUTORS.rst
@@ -1,1 +1,2 @@
 * Enric Tobella <etobella@creublanca.es>
+* Foram Shah <foram.shah@initos.com>
