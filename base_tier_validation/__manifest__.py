eb59cc54c1d76f042e278dc07d458714cbdc195c
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2021-09-27 15:54:24 +0200

    base_tier_validation: review_user_count perfromance

--- base_tier_validation/__manifest__.py
+++ base_tier_validation/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Base Tier Validation",
     "summary": "Implement a validation process based on tiers.",
-    "version": "14.0.2.5.0",
+    "version": "14.0.2.5.1",
     "development_status": "Mature",
     "maintainers": ["LoisRForgeFlow"],
     "category": "Tools",
