a177c98949edc27a6b39a3f27a9c70d475079ba5
sergio-teruel <sergio.teruel@tecnativa.com>
2021-10-02 08:56:57 +0200

    [FIX] base_tier_validation_formula: Fix cache miss error on iteration records

--- base_tier_validation_formula/models/tier_review.py
+++ base_tier_validation_formula/models/tier_review.py
@@ -29,7 +29,10 @@ class TierReview(models.Model):
 
     @api.depends("definition_id.reviewer_expression", "review_type", "model", "res_id")
     def _compute_python_reviewer_ids(self):
-        for rec in self.filtered(lambda x: x.review_type == "expression"):
+        for rec in self:
+            if rec.review_type != "expression":
+                rec.python_reviewer_ids = self.env["res.users"].browse()
+                continue
             record = rec.env[rec.model].browse(rec.res_id).exists()
             try:
                 reviewer_ids = safe_eval(
