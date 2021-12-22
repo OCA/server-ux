5f4ab0f02ca58a0bc6ddde18ba92317cb25fe381
JordiMForgeFlow <jordi.masvidal@forgeflow.com>
2021-12-22 11:09:47 +0100

    [13.0][ADD] base_custom_filter

---
+++ base_custom_filter/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Add custom filters in standard filters and group by dropdowns",
+    "version": "13.0.1.0.0",
+    "category": "Usability",
+    "website": "https://github.com/OCA/server-ux",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/ir_filters_views.xml",
+        "views/ir_filters_group_views.xml",
+    ],
+    "depends": ["web"],
+    "license": "AGPL-3",
+    "installable": True,
+    "maintainers": ["JordiMForgeFlow"],
+}
