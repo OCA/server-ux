3d405926286c2b181e6e87853af05852a5058bb5
JordiMForgeFlow <jordi.masvidal@forgeflow.com>
2022-02-21 08:19:16 +0100

    [FIX] base_user_locale: make new fields readable for user

--- base_user_locale/models/res_users.py
+++ base_user_locale/models/res_users.py
@@ -38,6 +38,23 @@ class ResUsers(models.Model):
         help="See Settings > Translations > Languages for details",
     )
 
+    def __init__(self, pool, cr):
+        """ Override of __init__ to add access rights.
+            Access rights are disabled by default, but allowed
+            on some specific fields defined in self.SELF_{READ/WRITE}ABLE_FIELDS.
+        """
+        base_user_locale_readable_fields = [
+            "date_format",
+            "time_format",
+            "week_start",
+            "decimal_point",
+            "thousands_sep",
+        ]
+        super(ResUsers, self).__init__(pool, cr)
+        type(self).SELF_READABLE_FIELDS = (
+            type(self).SELF_READABLE_FIELDS + base_user_locale_readable_fields
+        )
+
     def preference_save(self):
         super().preference_save()
         # Do a "full" reload instead of just a context_reload to apply locale
