8c40c907e830119afabdff1e53b9f2b15b7d09cc
Ashish Hirpara <ashish.hirapara1995@gmail.com>
2022-02-24 15:07:47 +0530

    [MIG] base_custom_filter: Migration to 14.0

--- base_custom_filter/__manifest__.py
+++ base_custom_filter/__manifest__.py
@@ -1,11 +1,12 @@
+# Migrated to v14.0 by Ashish Hirpara (https://www.ashish-hirpara.com)
 # Copyright 2021 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Add custom filters in standard filters and group by dropdowns",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Usability",
     "website": "https://github.com/OCA/server-ux",
-    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "author": "Ashish Hirpara, ForgeFlow, Odoo Community Association (OCA)",
     "data": [
         "security/ir.model.access.csv",
         "views/ir_filters_views.xml",
@@ -14,5 +15,5 @@
     "depends": ["web"],
     "license": "AGPL-3",
     "installable": True,
-    "maintainers": ["JordiMForgeFlow"],
+    "maintainers": ["AshishHirapara", "ForgeFlow"],
 }
