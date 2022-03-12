4ceaafb681c67585cdcc0c615f7b52f68ceda7a5
Saran440 <saranl@ecosoft.co.th>
2022-03-12 17:54:11 +0700

    [FIX] reject function only

--- base_tier_validation_server_action/wizard/comment_wizard.py
+++ base_tier_validation_server_action/wizard/comment_wizard.py
@@ -6,26 +6,22 @@ from odoo import models
 class CommentWizard(models.TransientModel):
     _inherit = "comment.wizard"
 
-    def _get_server_action(self, review):
-        """ get server action validate or reject """
-        validate_reject = self._context.get("default_validate_reject")
-        if validate_reject == "validate":
-            return review.definition_id.server_action_id
-        return review.definition_id.rejected_server_action_id
-
     def add_comment(self):
+        """ For case reject only """
         res = super().add_comment()
-        review = self.review_ids[-1]
-        server_action = self._get_server_action(review)
-        server_action_tier = self.env.context.get("server_action_tier")
-        # Don't allow reentrant server action as it will lead to
-        # recursive behaviour
-        if server_action and (
-            not server_action_tier or server_action_tier != server_action.id
-        ):
-            server_action.with_context(
-                server_action_tier=server_action.id,
-                active_model=review.model,
-                active_id=review.res_id,
-            ).sudo().run()
+        validate_reject = self._context.get("default_validate_reject")
+        if validate_reject == "reject":
+            review = self.review_ids[-1]
+            reject_server_action = review.definition_id.rejected_server_action_id
+            server_action_tier = self.env.context.get("server_action_tier")
+            # Don't allow reentrant server action as it will lead to
+            # recursive behaviour
+            if reject_server_action and (
+                not server_action_tier or server_action_tier != reject_server_action.id
+            ):
+                reject_server_action.with_context(
+                    server_action_tier=reject_server_action.id,
+                    active_model=review.model,
+                    active_id=review.res_id,
+                ).sudo().run()
         return res
