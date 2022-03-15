e8ced827c91dc35ed653f8817747c631cf9d3248
Christopher Ormaza <chris.ormaza@forgeflow.com>
2022-03-15 10:16:35 -0500

    [15.0][MIG] mass_operation_abstract

--- mass_operation_abstract/wizard/mass_operation_wizard_mixin.py
+++ mass_operation_abstract/wizard/mass_operation_wizard_mixin.py
@@ -49,17 +49,25 @@ class MassOperationWizardMixin(models.AbstractModel):
 
         if len(active_ids) == len(remaining_items):
             operation_description_info = _(
-                "The treatment will be processed on the %d selected elements."
-            ) % len(active_ids)
+                "The treatment will be processed on the %(len_active_ids)d selected elements."
+            ) % {
+                "len_active_ids": len(active_ids),
+            }
         elif len(remaining_items):
             operation_description_warning = _(
-                "You have selected %d items that can not be processed."
-                " Only %d items will be processed."
-            ) % (len(active_ids) - len(remaining_items), len(remaining_items))
+                "You have selected %(len_remaining_active_ids)d "
+                "items that can not be processed."
+                " Only %(len_remaining_items)d items will be processed."
+            ) % {
+                "len_remaining_active_ids": len(active_ids) - len(remaining_items),
+                "len_remaining_items": len(remaining_items),
+            }
         else:
             operation_description_danger = _(
-                "None of the %d items you have selected can be processed."
-            ) % len(active_ids)
+                "None of the %(len_active_ids)d items you have selected can be processed."
+            ) % {
+                "len_active_ids": len(active_ids),
+            }
 
         res.update(
             {
