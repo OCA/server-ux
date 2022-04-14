b1c0ee0eeb4a9e1d445a562dea3a0c0d45bac67d
Alex Cuellar <acuellar@grupoyacck.com>
2022-04-14 07:22:16 -0500

    [MIG] sequence_reset_period: Migration to 15.0

--- sequence_reset_period/__manifest__.py
+++ sequence_reset_period/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Reset Sequences on selected period ranges",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Reporting",
     "website": "https://github.com/OCA/server-ux",
     "author": "Creu Blanca, Odoo Community Association (OCA)",
