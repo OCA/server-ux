4ce86c77adaff0612dc9cfc17026f25e856c4cdf
Alexey Pelykh <alexey.pelykh@gmail.com>
2022-07-13 08:56:58 +0200

    [UPD] Brainbean Apps => CorporateHub

--- base_user_locale/__manifest__.py
+++ base_user_locale/__manifest__.py
@@ -1,10 +1,11 @@
 # Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
+# Copyright 2022 CorporateHub (https://corporatehub.eu)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "User Locale Settings",
     "version": "14.0.1.1.0",
-    "author": "Brainbean Apps, Odoo Community Association (OCA)",
+    "author": "CorporateHub, Odoo Community Association (OCA)",
     "category": "Usability",
     "license": "AGPL-3",
     "depends": ["base_setup", "calendar"],
