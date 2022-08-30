cb21c0bc6ba6bb644b4fa8a8692223ff1b162ba7
david <david.vidal@tecnativa.com>
2022-08-30 07:44:10 +0200

    [FIX] announcement: log read just once
    If a user has an announcement open in multiple windows, the read will be
    logged multiple times. The relevant one is only the first one.
    
    TT38705

--- announcement/__manifest__.py
+++ announcement/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Announcement",
-    "version": "13.0.2.0.0",
+    "version": "13.0.2.0.1",
     "summary": "Notify internal users about relevant organization stuff",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
