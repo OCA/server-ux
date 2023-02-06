b651ae19538db85845804d39707a81648a433691
JordiMForgeFlow <jordi.masvidal@forgeflow.com>
2023-02-06 12:53:06 +0100

    [ADD] base_action_visibility_restriction

---
+++ base_action_visibility_restriction/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Base Action Visibility Restriction",
+    "version": "13.0.1.0.0",
+    "category": "Extra Tools",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "summary": "Restrict (with groups) action visibilty",
+    "website": "https://github.com/OCA/server-ux",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "maintainers": ["JordiMForgeFlow"],
+    "data": ["views/ir_actions_views.xml"],
+    "installable": True,
+}
---
+++ base_action_visibility_restriction/readme/USAGE.rst
@@ -0,0 +1,7 @@
+To use this module, you need to:
+
+#. Activate the developer mode
+#. Go to *Settings > Technical > Actions*.
+#. Search for any server/window action and edit it.
+#. Update "Excluded groups" with one group.
+#. Login with a user of that group, and you won't see the action in the binding model.
