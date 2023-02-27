ecce75ebff47cd72756a573e53bfc71f71a9dbe6
Raúl Martín <raul.martin@braintec.com>
2023-02-27 11:39:48 +0100

    Improving conditions to double check if the operation is under validation

--- base_tier_validation/models/tier_validation.py
+++ base_tier_validation/models/tier_validation.py
@@ -243,7 +243,9 @@ class TierValidation(models.AbstractModel):
                         )
                     )
             if (
+                # Only check if a state change is included in vals                
                 rec.review_ids
+                and self._state_field in vals
                 and getattr(rec, self._state_field) in self._state_from
                 and not vals.get(self._state_field)
                 in (self._state_to + [self._cancel_state])
