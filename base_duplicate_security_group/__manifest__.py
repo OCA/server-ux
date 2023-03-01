96b11756396b4aed9aa03859a4e74bb262932f6d
David <david.vidal@tecnativa.com>
2023-03-01 18:19:56 +0100

    [MIG] base_duplicate_security_group: Migration to 15.0
    TT37272

--- base_duplicate_security_group/__manifest__.py
+++ base_duplicate_security_group/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Restrict records duplicating",
     "summary": "Adds a security group to restrict which users can copy records",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "development_status": "Beta",
     "category": "Server tools",
     "website": "https://github.com/OCA/server-ux",
@@ -11,8 +11,13 @@
     "maintainers": ["chienandalu"],
     "license": "AGPL-3",
     "depends": ["web_tour"],
-    "data": [
-        "security/base_duplicate_security_group_security.xml",
-        "views/assets.xml",
-    ],
+    "data": ["security/base_duplicate_security_group_security.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "/base_duplicate_security_group/static/src/js/duplicate.esm.js",
+        ],
+        "web.assets_tests": [
+            "/base_duplicate_security_group/static/src/js/tour_duplicate.esm.js",
+        ],
+    },
 }
