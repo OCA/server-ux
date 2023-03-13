252b25f4b3cb0817758ca023fae3dc7b8c0ba283
Weblate <noreply@weblate.org>
2023-03-13 12:11:33 +0000

    [MIG] base_revision: Migration to 16.0 [UPD] Update base_revision.pot [UPD] README.rst Update translation files
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: server-ux-16.0/server-ux-16.0-base_revision
    Translate-URL: https://translation.odoo-community.org/projects/server-ux-16-0/server-ux-16-0-base_revision/

--- base_revision/__manifest__.py
+++ base_revision/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Base Revision (abstract)",
     "summary": "Keep track of revised document",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Tools",
     "author": "Agile Business Group,"
     "Dreambits,"
