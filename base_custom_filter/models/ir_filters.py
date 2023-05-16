54789424291bb6c91d07abf74d94db4e05e7c3bb
Víctor Martínez <victor.martinez@tecnativa.com>
2023-05-16 13:08:02 +0200

    [FIX] base_custom_filter: Prevent all records from being displayed in favorites
    TT43350

--- base_custom_filter/models/ir_filters.py
+++ base_custom_filter/models/ir_filters.py
@@ -2,7 +2,8 @@
 # Copyright 2021 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.osv import expression
 
 
 class IrFilters(models.Model):
@@ -29,3 +30,19 @@ class IrFilters(models.Model):
         ondelete="cascade",
     )
     group_id = fields.Many2one(comodel_name="ir.filters.group", string="Filter Group")
+
+    @api.model
+    def get_filters(self, model, action_id=None):
+        """We need to inject a context to obtain only the records of favorite type."""
+        self = self.with_context(filter_type="favorite")
+        return super().get_filters(model, action_id=action_id)
+
+    @api.model
+    def search(self, args, offset=0, limit=None, order=None, count=False):
+        if self.env.context.get("filter_type"):
+            args = expression.AND(
+                (args, [("type", "=", self.env.context["filter_type"])])
+            )
+        return super().search(
+            args, offset=offset, limit=limit, order=order, count=count
+        )
