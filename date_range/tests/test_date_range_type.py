2f334b51eae2a7b3e35e5f72b3611633de9a707d
Alexis de Lattre <alexis.delattre@akretion.com>
2023-06-16 15:12:43 +0200

    date_range: AGPL -> LGPL

--- date_range/tests/test_date_range_type.py
+++ date_range/tests/test_date_range_type.py
@@ -1,5 +1,5 @@
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
 
 from dateutil.relativedelta import relativedelta
 from dateutil.rrule import MONTHLY, YEARLY
