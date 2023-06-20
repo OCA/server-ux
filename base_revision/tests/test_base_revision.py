7c0af4bc0e2894275b9ff16caeb50887d2e6c630
Christopher Rogos <christopher.rogos@glueckkanja-gab.com>
2023-06-20 10:55:48 +0000

    [FIX] warning create method in batch

--- base_revision/tests/test_base_revision.py
+++ base_revision/tests/test_base_revision.py
@@ -88,3 +88,13 @@ class TestBaseRevision(common.TransactionCase):
         tester_3 = tester_2.copy({"name": "TEST0002"})
         # Check the 'Reference' of the copied tester
         self.assertEqual(tester_3.name, tester_3.unrevisioned_name)
+
+    def test_create_multiple(self):
+        """Check copy process"""
+        # Create a tester
+        tester_2 = self.revision_model.create(
+            [{"name": "TEST0001"}, {"name": "TEST0002"}]
+        )
+        # Check the 'Order Reference' of the tester
+        for tester in tester_2:
+            self.assertEqual(tester.name, tester.unrevisioned_name)
