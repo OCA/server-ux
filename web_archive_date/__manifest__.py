4f4734e1990ca4e59a305c7e793d657412b2f7c4
GuillemCForgeFlow <guillem.casassas@forgeflow.com>
2023-06-26 14:02:16 +0200

    [ADD]web_archive_date Glue module between base_archive_date and web to display the Latest Archived Date and Latest Archived by on the metadata of a record.

---
+++ web_archive_date/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Web Archive Date",
+    "summary": """
+        Reflects the Latest Archived Date and Latest Archived by on the record metadata.
+    """,
+    "version": "13.0.1.0.0",
+    "category": "Usability",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/server-ux",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "depends": ["web", "base_archive_date"],
+    "maintainers": ["GuillemCForgeFlow"],
+    "qweb": ["static/src/xml/debug.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": True,
+}
