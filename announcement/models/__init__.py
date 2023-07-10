514ffd26ea8f7b392bb82817620087c2d78895c8
Víctor Martínez <victor.martinez@tecnativa.com>
2023-07-10 12:41:23 +0200

    [IMP] announcement: Add Announcement Tags
    TT43045

--- announcement/models/__init__.py
+++ announcement/models/__init__.py
@@ -1,3 +1,4 @@
 from . import announcement
+from . import announcement_tag
 from . import ir_config_parameter
 from . import res_users
---
+++ announcement/models/announcement_tag.py
@@ -0,0 +1,47 @@
+# Copyright 2023 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class AnnouncementTag(models.Model):
+    _name = "announcement.tag"
+    _description = "Announcement Tags"
+
+    name = fields.Char(required=True, translate=True)
+    color = fields.Integer()
+    parent_id = fields.Many2one(
+        comodel_name="announcement.tag",
+        string="Parent Tag",
+        index=True,
+        ondelete="cascade",
+    )
+    full_name = fields.Char(compute="_compute_full_name")
+    company_id = fields.Many2one(
+        comodel_name="res.company",
+        string="Company",
+        required=True,
+        index=True,
+        default=lambda self: self.env.company,
+        help="Company related to this tag",
+    )
+
+    _sql_constraints = [("name_uniq", "unique (name)", "Tag name already exists!")]
+
+    @api.constrains("parent_id")
+    def _check_parent_id(self):
+        if not self._check_recursion():
+            raise ValidationError(_("You cannot create recursive tags."))
+
+    @api.depends("parent_id", "name")
+    def _compute_full_name(self):
+        for item in self:
+            item.full_name = (
+                item.parent_id.name + " / " + item.name if item.parent_id else item.name
+            )
+
+    def name_get(self):
+        res = []
+        for item in self:
+            res.append((item.id, item.full_name))
+        return res
