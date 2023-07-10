cae56b8b1605498a73221df749b29e4e412c13cc
Víctor Martínez <victor.martinez@tecnativa.com>
2023-07-10 12:41:23 +0200

    [IMP] announcement: Add Announcement Tags
    TT43045

--- announcement/__manifest__.py
+++ announcement/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Announcement",
-    "version": "15.0.1.1.0",
+    "version": "15.0.1.2.0",
     "summary": "Notify internal users about relevant organization stuff",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
@@ -14,7 +14,11 @@
         "security/announcement_security.xml",
         "security/ir.model.access.csv",
         "views/announcement_views.xml",
+        "views/announcement_tag_views.xml",
         "wizards/read_announcement_wizard.xml",
+    ],
+    "demo": [
+        "demo/announcement_tag_demo.xml",
     ],
     "assets": {
         "web.assets_backend": ["announcement/static/src/**/*.js"],
