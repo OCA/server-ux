aa5652184bed3cdd0dd8b43f872c5c599f56d3d4
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-07-27 10:02:29 +0200

    [FIX] announcement: Avoid permission error on attachments
    Doing them to be owned by the record.

--- announcement/__manifest__.py
+++ announcement/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Announcement",
-    "version": "15.0.1.2.1",
+    "version": "15.0.2.0.0",
     "summary": "Notify internal users about relevant organization stuff",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
