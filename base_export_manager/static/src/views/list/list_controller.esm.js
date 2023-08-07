4252afe96cd9efd8e7439380979fa12cba40a778
Stefan <stefan.ungureanu@tecnativa.com>
2023-08-07 07:00:31 +0200

    [FIX] base_export_manager: ListController corrected file

--- base_export_manager/static/src/views/list/list_controller.esm.js
+++ base_export_manager/static/src/views/list/list_controller.esm.js
@@ -1,15 +1,15 @@
 /** @odoo-module **/
 
 import {ListController} from "@web/views/list/list_controller";
-import {onWillRender} from "@odoo/owl";
+const {onWillRender} = owl;
 import {patch} from "@web/core/utils/patch";
 import {session} from "@web/session";
 
 patch(ListController.prototype, "base_export_manager", {
-    setup(){
+    setup() {
         this._super(...arguments);
         onWillRender(async () => {
-            if (this.isExportEnable){
+            if (this.isExportEnable) {
                 const is_export_enabled =
                     session.export_models.indexOf(this.model.root.resModel) !== -1;
                 if (!session.is_system && !is_export_enabled) {
@@ -17,5 +17,5 @@ patch(ListController.prototype, "base_export_manager", {
                 }
             }
         });
-    }
+    },
 });
