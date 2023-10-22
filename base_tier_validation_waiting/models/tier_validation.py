4a073ce35f40680e3484c788fdbcc4deb0e45e4a
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2023-10-22 18:42:35 +0200

    [IMP] base_tier_validation_waiting: code refactor

--- base_tier_validation_waiting/models/tier_validation.py
+++ base_tier_validation_waiting/models/tier_validation.py
@@ -12,24 +12,20 @@ class TierValidation(models.AbstractModel):
         subscribe = "message_subscribe"
         post = "message_post"
         if hasattr(self, post) and hasattr(self, subscribe):
-            for rec in self:
+            for rec in self.sudo():
                 users_to_notify = tier_reviews.filtered(
                     lambda r: r.definition_id.notify_on_pending and r.res_id == rec.id
                 ).mapped("reviewer_ids")
                 # Subscribe reviewers and notify
-                getattr(rec.sudo(), subscribe)(
+                getattr(rec, subscribe)(
                     partner_ids=users_to_notify.mapped("partner_id").ids
                 )
-                getattr(rec.sudo(), post)(
+                getattr(rec, post)(
                     subtype_xmlid=self._get_requested_notification_subtype(),
                     body=rec._notify_requested_review_body(),
                 )
 
-    def _validate_tier(self, tiers=False):
-        """extend base tier validation method to allow bypass of waiting
-        sequences as pending state is hardcoded"""
-        self.ensure_one()
-        super(TierValidation, self)._validate_tier(tiers)
+    def _validate_tier_waiting_reviews(self, tiers=False):
         tier_reviews = tiers or self.review_ids
         waiting_reviews = tier_reviews.filtered(
             lambda r: r.status == "waiting"
@@ -47,3 +43,11 @@ class TierValidation(models.AbstractModel):
             for review in waiting_reviews:
                 rec = self.env[review.model].browse(review.res_id)
                 rec._notify_accepted_reviews()
+
+    def _validate_tier(self, tiers=False):
+        """extend base tier validation method to allow bypass of waiting
+        sequences as pending state is hardcoded"""
+        self.ensure_one()
+        self._validate_tier_waiting_reviews(tiers)
+        res = super(TierValidation, self)._validate_tier(tiers)
+        return res
