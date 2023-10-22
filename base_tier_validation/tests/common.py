71cfed26b8e2f582a50642e784e6b99415b417fa
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2023-10-22 19:51:20 +0200

    [IMP] base_tier_validation: update tests to support messages

--- base_tier_validation/tests/common.py
+++ base_tier_validation/tests/common.py
@@ -58,10 +58,15 @@ class CommonTierValidation(common.TransactionCase):
         # Create users:
         group_ids = cls.env.ref("base.group_system").ids
         cls.test_user_1 = cls.env["res.users"].create(
-            {"name": "John", "login": "test1", "groups_id": [(6, 0, group_ids)]}
+            {
+                "name": "John",
+                "login": "test1",
+                "email": "john@yourcompany.example.com",
+                "groups_id": [(6, 0, group_ids)],
+            }
         )
         cls.test_user_2 = cls.env["res.users"].create(
-            {"name": "Mike", "login": "test2"}
+            {"name": "Mike", "login": "test2", "email": "mike@yourcompany.example.com"}
         )
 
         # Create tier definitions:
