0f33b29277a5b70efb33e48f5f14459efcf9d43c
Víctor Martínez <victor.martinez@tecnativa.com>
2023-11-15 09:57:51 +0100

    [IMP] base_tier_validation: Add _prepare_tier_review_vals() methot to allow inheritance
    TT45804

--- base_tier_validation/tests/common.py
+++ base_tier_validation/tests/common.py
@@ -9,8 +9,17 @@ from odoo.tests import common
 class CommonTierValidation(common.SavepointCase):
     @classmethod
     def setUpClass(cls):
-        super(CommonTierValidation, cls).setUpClass()
-
+        super().setUpClass()
+        # Remove this variable in v16 and put instead:
+        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+        DISABLED_MAIL_CONTEXT = {
+            "tracking_disable": True,
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         cls.loader = FakeModelLoader(cls.env, cls.__module__)
         cls.loader.backup_registry()
         from .tier_validation_tester import TierValidationTester, TierValidationTester2
