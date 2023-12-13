868c521ce2a66514a0695e11e027628087180dc3
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-12-13 14:39:20 +0530

    [ADD] base_tier_validation_definition_server_action

---
+++ base_tier_validation_definition_server_action/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 ForgeFlow S.L. (http://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Base Tier Validation Defination Server Action",
+    "summary": """Server action for Base tier validation""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ForgeFlow,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-ux",
+    "depends": ["base_tier_validation"],
+    "data": ["views/tier_definition_view.xml"],
+    "demo": [],
+}
