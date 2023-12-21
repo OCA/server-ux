40d44ea647327ebcf38df89071c24ec79802c4b8
David <david.vidal@tecnativa.com>
2023-12-21 11:34:53 +0100

    [FIX] announcement: allow multicompany tags

--- announcement/__manifest__.py
+++ announcement/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Announcement",
-    "version": "15.0.2.1.0",
+    "version": "15.0.2.1.1",
     "summary": "Notify internal users about relevant organization stuff",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
