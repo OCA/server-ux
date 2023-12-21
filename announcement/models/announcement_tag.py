603bdb615eb43a1081c2a3b8ea84fb6b893c3936
David <david.vidal@tecnativa.com>
2023-12-21 11:34:53 +0100

    [FIX] announcement: allow multicompany tags

--- announcement/models/announcement_tag.py
+++ announcement/models/announcement_tag.py
@@ -20,9 +20,7 @@ class AnnouncementTag(models.Model):
     company_id = fields.Many2one(
         comodel_name="res.company",
         string="Company",
-        required=True,
         index=True,
-        default=lambda self: self.env.company,
         help="Company related to this tag",
     )
 
