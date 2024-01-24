a019e53aa8444cbeb0f9f42cb32180084d49981a
evanomnisoft <103913060+evanomnisoft@users.noreply.github.com>
2024-01-24 11:06:26 +0800

    [FIX] Incorrect displayed timezone for tier review validation date

--- base_tier_validation/models/tier_review.py
+++ base_tier_validation/models/tier_review.py
@@ -71,7 +71,7 @@ class TierReview(models.Model):
             if not review.reviewed_date:
                 review.reviewed_formated_date = False
                 continue
-            requested_date_utc = pytz.timezone(timezone).localize(review.reviewed_date)
+            requested_date_utc = pytz.timezone("UTC").localize(review.reviewed_date)
             requested_date = requested_date_utc.astimezone(pytz.timezone(timezone))
             review.reviewed_formated_date = requested_date.replace(tzinfo=None)
 
