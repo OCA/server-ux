46063aaab939f5dd697bf6fb11b56141a6775cb3
Florent de Labarre <florent.mirieu@gmail.com>
2024-02-23 09:26:11 +0100

    [FIX] base_tier_validation: prevent useless loop

--- base_tier_validation/models/res_users.py
+++ base_tier_validation/models/res_users.py
@@ -33,10 +33,10 @@ class Users(models.Model):
                     records = records.filtered(
                         lambda x: x[x._state_field] != x._cancel_state
                     )
-                for rec in records:
+                if records:
                     record = self.env[model]
                     user_reviews[model] = {
-                        "id": rec.id,
+                        "id": records[0].id,
                         "name": record._description,
                         "model": model,
                         "active_field": "active" in record._fields,
