7a7554d1d8c08161f3e42c8d816df5c1b04b0855
siemenv <siemen.vandervoort@dynapps.be>
2024-03-04 08:17:17 +0100

    [MIG] base_revision: Migration to 17.0

--- base_revision/__manifest__.py
+++ base_revision/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Base Revision (abstract)",
     "summary": "Keep track of revised document",
-    "version": "16.0.1.0.2",
+    "version": "17.0.1.0.0",
     "category": "Tools",
     "author": "Agile Business Group,"
     "Dreambits,"
