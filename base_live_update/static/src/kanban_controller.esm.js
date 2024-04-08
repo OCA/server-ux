e797ecc33c73d5c8a62d65c2d23dd369ac2d0061
Holger Brunn <mail@hunki-enterprises.com>
2024-04-08 22:52:53 +0200

    fixup! [ADD] base_live_update

--- base_live_update/static/src/kanban_controller.esm.js
+++ base_live_update/static/src/kanban_controller.esm.js
@@ -1,5 +1,6 @@
 /** @odoo-module **/
 
+import {Domain} from "@web/core/domain";
 import {KanbanController} from "@web/views/kanban/kanban_controller";
 import {patch} from "web.utils";
 import {useLiveUpdate} from "@base_live_update/base_live_update.esm";
@@ -16,10 +17,22 @@ patch(KanbanController.prototype, "kanban_live_update", {
             case "write": {
                 const updated = records.filter((r) => detail.__ids.includes(r.resId));
                 for (const record of updated) {
-                    await record.load();
-                    // TODO the below would be nicer, but then we need to
-                    // deal with nonstored fields, and convert values
-                    // await record.update(detail);
+                    await this.model.mutex.exec(async () => {
+                        const group = (this.model.root.groups || []).find((g) =>
+                            g.records.includes(record)
+                        );
+                        await record.load();
+                        // TODO the below would be nicer, but then we need to
+                        // deal with nonstored fields, and convert values
+                        // await record.update(detail);
+                        const new_group = (this.model.root.groups || []).find((g) =>
+                            new Domain(g.groupDomain).contains(record.dataContext)
+                        );
+                        if (group && new_group && new_group !== group) {
+                            group.records.splice(group.records.indexOf(record), 1);
+                            new_group.records.splice(0, 0, record);
+                        }
+                    });
                 }
                 break;
             }
@@ -31,18 +44,18 @@ patch(KanbanController.prototype, "kanban_live_update", {
                 break;
             }
             case "create": {
-                for (const [resId] of detail) {
-                    // TODO this should check if the new record is entailed by the
-                    // currently active domain
-                    this.model.root.addExistingRecord(resId);
+                for (const [resId, data] of detail) {
+                    for (const group of this.model.root.groups || [this.model.root]) {
+                        if (
+                            new Domain(group.groupDomain || group.domain).contains(data)
+                        ) {
+                            await group.addExistingRecord(resId);
+                            break;
+                        }
+                    }
                 }
                 break;
             }
-        }
-        for (const group of this.model.root.groups || []) {
-            // TODO this should be smarter and add/remove records
-            // to/from groups based on the updates we did above
-            await group.load();
         }
         this.model.notify();
         return this.render(true);
