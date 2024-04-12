87e9cd4b95fe37afc1866dd399cde73a6cff825e
Wodran Van de Sande <wodran.vandesande@dynapps.be>
2024-04-12 14:51:10 +0200

    [MIG] date_range_account: Migration to 17.0

--- date_range_account/__manifest__.py
+++ date_range_account/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Date Range Account",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "summary": "Add Date Range menu entry in Invoicing app",
