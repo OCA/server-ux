4e588de9dc785fa77a66c566d8dbcbc02a6a144f
Thierry Ducrest <thierry@ducrest.net>
2024-04-29 10:07:37 +0200

    [IMP] base_tier_validation: Add reminder option

--- base_tier_validation/models/tier_definition.py
+++ base_tier_validation/models/tier_definition.py
@@ -94,6 +94,11 @@ class TierDefinition(models.Model):
         "to this definition are restarted.",
     )
     has_comment = fields.Boolean(string="Comment", default=False)
+    notify_reminder_delay = fields.Integer(
+        string="Send reminder message on pending reviews",
+        help="Number of days after which a message must be posted to remind about "
+        "pending validation  (0 = no reminder)",
+    )
     approve_sequence = fields.Boolean(
         string="Approve by sequence",
         default=False,
@@ -117,3 +122,33 @@ class TierDefinition(models.Model):
                 .sudo()
                 .search([("model", "=", rec.model), ("relation", "=", "res.users")])
             )
+
+    def _get_review_needing_reminder(self):
+        """Return all the reviews that have the reminder setup."""
+        self.ensure_one()
+        if not self.notify_reminder_delay:
+            return self.env["tier.review"]
+        review_date = fields.Datetime.subtract(
+            fields.Datetime.now(), days=self.notify_reminder_delay
+        )
+        return self.env["tier.review"].search(
+            [
+                ("definition_id", "=", self.id),
+                ("status", "=", "pending"),
+                "|",
+                "&",
+                ("create_date", "<", review_date),
+                ("last_reminder_date", "=", False),
+                ("last_reminder_date", "<", review_date),
+            ],
+            limit=1,
+        )
+
+    def _cron_send_review_reminder(self):
+        definition_with_reminder = self.env["tier.definition"].search(
+            [("notify_reminder_delay", ">", 0)]
+        )
+        for record in definition_with_reminder:
+            review_to_remind = record._get_review_needing_reminder()
+            if review_to_remind:
+                review_to_remind._send_review_reminder()
