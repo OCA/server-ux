c693edda324f94becd2b59929bdea5486954fa38
David Beal <david.beal@akretion.com>
2024-05-10 13:54:31 +0200

    [ADD][16.0] developer_menu module

---
+++ developer_menu/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Developer Menu",
+    "version": "16.0.1.0.0",
+    "category": "Tools",
+    "license": "AGPL-3",
+    "summary": "Menu Shortcut for developer usage",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-ux",
+    "maintainers": ["bealdav"],
+    "depends": [
+        "mail",
+    ],
+    "data": [
+        "views/menu_view.xml",
+    ],
+    "installable": True,
+}
---
+++ developer_menu/readme/USAGE.rst
@@ -0,0 +1,5 @@
+Ensure you're in ERP manager group and go to configuration page
+near `Technical` menu and click on 🧰
+
+.. figure:: ../static/description/menu.png
+    :alt: Menu
