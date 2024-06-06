b87a284ac27374cd00ae986a4d8092efcf0cdb2f
David Beal <david.beal@akretion.com>
2024-06-06 12:48:40 +0200

    [ADD][16.0] module misc_settings

---
+++ misc_settings/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Miscellaneous Settings",
+    "version": "16.0.1.0.0",
+    "category": "Tools",
+    "license": "AGPL-3",
+    "summary": "Miscellaneous settings to be an anchor for your custom settings",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-ux",
+    "maintainers": ["bealdav"],
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/res_config_settings.xml",
+    ],
+    "installable": True,
+}
