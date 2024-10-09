ca42a6efefb876be84b2ad292b60713d4fef6fd0
David Beal <david.beal@akretion.com>
2024-10-09 15:14:00 +0200

    [MIG] developer_menu: Migration to 18.0

--- developer_menu/__manifest__.py
+++ developer_menu/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Developer Menu",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Tools",
     "license": "AGPL-3",
     "summary": "Menu Shortcut for developer usage",
