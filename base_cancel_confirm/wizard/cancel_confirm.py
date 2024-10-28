b7802929696faee67bc4822fd6a8b27d84b60aa7
GuillemCForgeFlow <guillem.casassas@forgeflow.com>
2024-10-28 13:49:41 +0100

    [IMP]base_cancel_confirm: black, isort, prettier

--- base_cancel_confirm/wizard/cancel_confirm.py
+++ base_cancel_confirm/wizard/cancel_confirm.py
@@ -7,15 +7,9 @@ class CancelConfirm(models.TransientModel):
     _name = "cancel.confirm"
     _description = "Cancel Confirm"
 
-    cancel_reason = fields.Text(
-        string="Cancel Reason",
-    )
+    cancel_reason = fields.Text(string="Cancel Reason",)
     has_cancel_reason = fields.Selection(
-        selection=[
-            ("no", "None"),
-            ("optional", "Optional"),
-            ("required", "Required"),
-        ],
+        selection=[("no", "None"), ("optional", "Optional"), ("required", "Required"),],
         default="no",
         required=True,
     )
