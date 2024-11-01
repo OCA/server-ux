19cc8f07c4e1b225bc6a76fdec280a59bad29502
Kevin Khao <kevinkhao@gmail.com>
2024-11-01 15:15:45 +0200

    [IMP] base_tier_validation: pre-commit auto fixes

--- base_tier_validation/__manifest__.py
+++ base_tier_validation/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Base Tier Validation",
     "summary": "Implement a validation process based on tiers.",
-    "version": "17.0.2.2.1",
+    "version": "18.0.1.0.1",
     "development_status": "Mature",
     "maintainers": ["LoisRForgeFlow"],
     "category": "Tools",
