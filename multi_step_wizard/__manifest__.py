87f3cba064cee2d58b9e8dfce22e195f7af6196a
Sander Lienaerts <sander.lienaerts@codeforward.nl>
2024-11-28 09:03:12 +0100

    [MIG] multi_step_wizard: Migration to 18.0

--- multi_step_wizard/__manifest__.py
+++ multi_step_wizard/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Multi-Steps Wizards",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["base"],
