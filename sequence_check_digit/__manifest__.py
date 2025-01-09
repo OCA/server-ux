3817de69aefd9a98f8866aa6162e52331c28a17e
Maksym Yankin <yankinmk@gmail.com>
2025-01-09 13:33:09 +0200

    [MIG] sequence_check_digit: Migration to 18.0

--- sequence_check_digit/__manifest__.py
+++ sequence_check_digit/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Check Digit on Sequences",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Reporting",
     "website": "https://github.com/OCA/server-ux",
     "author": "Creu Blanca, Odoo Community Association (OCA)",
