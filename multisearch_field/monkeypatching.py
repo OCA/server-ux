60e26e3096d279e99e78faa4cfffd7e2d0b2bbdc
TB-Ph35 <thomas.bonnerue@akretion.com>
2025-01-26 23:21:00 +0100

    [FIX] Goupby by modify _where_calc

--- multisearch_field/monkeypatching.py
+++ multisearch_field/monkeypatching.py
@@ -6,10 +6,10 @@ import re
 from odoo.models import BaseModel
 from odoo.osv import expression
 
-search_original = BaseModel.search
+_where_calc_orginal = BaseModel._where_calc
 
 
-def search(self, domain, offset=0, limit=None, order=None, count=False):
+def _where_calc(self, domain, active_test=True):
     """Override of the Python method to remove the dependency of the unit
     fields"""
     if self.check_access_rights("read") and self != self.env["ir.config_parameter"]:
@@ -19,10 +19,11 @@ def search(self, domain, offset=0, limit=None, order=None, count=False):
     else:
         list_separator = False
     if not list_separator or not domain:
-        return search_original(self, domain, offset, limit, order, count)
+        return _where_calc_orginal(self, domain, active_test)
 
     domain_list = []
     domain_value = []
+    domain_list_value = []
     nlist_sep = []
     for sep in list_separator:
         nlist_sep.append(sep)
@@ -43,12 +44,15 @@ def search(self, domain, offset=0, limit=None, order=None, count=False):
             for value in value_list_set:
                 domain_search_field = [(dom[0], dom[1], value)]
                 domain_value.append(domain_search_field)
+            domain_list_value = expression.OR(domain_value)
+            for item in domain_list_value:
+                domain_list.append(item)
+            domain_value = []
+            domain_list_value = []
         else:
             domain_list.append(dom)
-    if domain_value:
-        domain_list = expression.OR(domain_value)
-    res = search_original(self, domain_list, offset, limit, order, count)
+    res = _where_calc_orginal(self, domain_list, active_test)
     return res
 
 
-BaseModel.search = search
+BaseModel._where_calc = _where_calc
