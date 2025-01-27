a4358ad1018c9a671f7e91b1ffeda10c87b95735
jmartinacu <jmartinacu2002@gmail.com>
2025-01-27 17:27:51 +0100

    [MIG] barcode_action: Imported rpc util from '@web/core/network/rpc' in js

--- barcode_action/static/src/js/action_barcode_widget.esm.js
+++ barcode_action/static/src/js/action_barcode_widget.esm.js
@@ -1,20 +1,20 @@
 import {registry} from "@web/core/registry";
 import {standardFieldProps} from "@web/views/fields/standard_field_props";
 import {useBus, useService} from "@web/core/utils/hooks";
+import { rpc } from "@web/core/network/rpc";
 
 import {Component, xml} from "@odoo/owl";
 
 export class ActionBarcodeField extends Component {
     setup() {
         const barcode = useService("barcode");
-        this.rpc = useService("rpc");
         useBus(barcode.bus, "barcode_scanned", this.onBarcodeScanned);
     }
     onBarcodeScanned(event) {
         const {barcode} = event.detail;
         var self = this;
         var record = this.props.record;
-        this.rpc(`/web/dataset/call_kw/${record.data.model}/${record.data.method}`, {
+        rpc(`/web/dataset/call_kw/${record.data.model}/${record.data.method}`, {
             model: record.data.model,
             method: record.data.method,
             args: [[record.data.res_id], barcode],
