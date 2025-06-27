93274791acd38a7d3db43e7f582569cc7dc4a509
David Bañón <david.banon@tecnativa.com>
2025-06-27 12:39:13 +0200

    [MIG] announcement_dialog_size: Migration to 18.0
    TT55500

--- announcement_dialog_size/__manifest__.py
+++ announcement_dialog_size/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Announcement Dialog Size",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "summary": "Allow set announcement dialogs fullsized by default",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
--- announcement_dialog_size/static/src/js/announcement_menu/announcement_menu.esm.js
+++ announcement_dialog_size/static/src/js/announcement_menu/announcement_menu.esm.js
@@ -4,9 +4,9 @@
 import {AnnouncementMenu} from "@announcement/js/announcement_menu/announcement_menu.esm";
 import {patch} from "@web/core/utils/patch";
 
-patch(AnnouncementMenu.prototype, "announcement_dialog_size.AnnouncementMenu", {
-    async getDialogAnnouncementProps() {
-        const props = await this._super(...arguments);
+patch(AnnouncementMenu.prototype, {
+    async getDialogAnnouncementProps(announcement) {
+        const props = await super.getDialogAnnouncementProps(announcement);
         const fullsize = await this.orm.call("announcement", "announcement_full_size");
         if (fullsize) {
             props.size = "dialog_full_screen";
