3d1444b4b372c00d8409880512677e2286123e27
Nikos Tsirintanis <ntsirintanis@therp.nl>
2025-09-05 11:06:44 +0200

    [MIG] base_tier_validation_report: Migration to 18.0

--- base_tier_validation_report/models/tier_review.py
+++ base_tier_validation_report/models/tier_review.py
@@ -16,14 +16,15 @@ class TierReview(models.Model):
     )
     resource_type = fields.Selection(
         selection="_selection_resource_ref",
+        compute="_compute_display_fields",
         compute_sudo=True,
     )
     resource_name = fields.Char(
-        compute="_compute_resource_ref",
+        compute="_compute_display_fields",
         compute_sudo=True,
     )
     next_review = fields.Char(
-        compute="_compute_resource_ref",
+        compute="_compute_display_fields",
         compute_sudo=True,
     )
 
@@ -35,12 +36,14 @@ class TierReview(models.Model):
     @api.depends("model", "res_id")
     def _compute_resource_ref(self):
         for rec in self:
-            rec.resource_ref = (
-                "%s,%s" % (rec.model, rec.res_id) if rec.res_id else False
-            )
-            rec.resource_name = rec.resource_ref.display_name
+            rec.resource_ref = f"{rec.model},{rec.res_id}" if rec.res_id else False
+
+    @api.depends("model", "res_id", "resource_ref")
+    def _compute_display_fields(self):
+        for rec in self:
             rec.resource_type = rec.model
-            rec.next_review = rec.resource_ref.next_review
+            rec.resource_name = rec.resource_ref.display_name
+            rec.next_review = rec.next_review
 
     def action_open_resource_ref(self):
         self.ensure_one()
