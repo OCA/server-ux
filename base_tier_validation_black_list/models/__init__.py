8a8a7b8b0068839cad9c20b10a10e8029890fb42
César León <cesar.leon@trescloud.com>
2025-12-15 12:01:29 -0500

    [18.0][ADD] Fields like blacklist in tier exeptions

---
+++ base_tier_validation_black_list/models/__init__.py
@@ -0,0 +1,2 @@
+from . import tier_validation
+from . import tier_validation_exception
---
+++ base_tier_validation_black_list/models/tier_validation_exception.py
@@ -0,0 +1,15 @@
+from odoo import fields, models
+
+
+class TierValidationException(models.Model):
+    _inherit = "tier.validation.exception"
+
+    is_blacklist = fields.Boolean(
+        string="Is Blacklist Exception",
+        help="If checked, the selected fields will be skiped "
+        "in allowed fields for tier validation.",
+    )
+    field_ids = fields.Many2many(
+        required=False, # Se lo hará requerido siempre y cuando is_blacklist no esté activo
+        # Esto permite que el modelo sea editable en cualquier momento de la validación
+    )
