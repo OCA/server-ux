87a1a9f6d193025d9fee1531ccac1c42c16b505a
bfacu <facundo.barral@eynes.com.ar>
2026-01-12 11:22:26 -0300

    [MIG] multi_step_wizard: Migration to 19.0

--- multi_step_wizard/tests/common.py
+++ multi_step_wizard/tests/common.py
@@ -1,8 +1,7 @@
 # Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo_test_helper import FakeModelLoader
-
+from odoo.orm.model_classes import add_to_registry
 from odoo.tests import common
 
 
@@ -10,13 +9,12 @@ class CommonTestMultiStepWizard(common.TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.loader = FakeModelLoader(cls.env, cls.__module__)
-        cls.loader.backup_registry()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         from .multi_step_wizard_test import MultiStepWizardTest
 
-        cls.loader.update_registry((MultiStepWizardTest,))
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.loader.restore_registry()
-        return super().tearDownClass()
+        add_to_registry(cls.registry, MultiStepWizardTest)
+        cls.registry._setup_models__(cls.env.cr, ["multi.step.wizard.test"])
+        cls.registry.init_models(
+            cls.env.cr, ["multi.step.wizard.test"], {"models_to_check": True}
+        )
+        cls.addClassCleanup(cls.registry.__delitem__, "multi.step.wizard.test")
